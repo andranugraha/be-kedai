c8b30c80669d098798912cd16d17fdf100b48ab8
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-02-20 21:24:45 +0700

    feat(user): error validator tag

--- internal/utils/response/response.go
+++ internal/utils/response/response.go
@@ -1,17 +1,20 @@
 package response
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
-type response struct {
+type Response struct {
 	Code    string      `json:"code"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data,omitempty"`
 }
 
 func Success(c *gin.Context, statusCode int, code, message string, data interface{}) {
-	c.JSON(statusCode, response{
+	c.JSON(statusCode, Response{
 		Code:    code,
 		Message: message,
 		Data:    data,
@@ -19,8 +22,28 @@ func Success(c *gin.Context, statusCode int, code, message string, data interfac
 }
 
 func Error(c *gin.Context, statusCode int, code, message string) {
-	c.JSON(statusCode, response{
+	c.JSON(statusCode, Response{
 		Code:    code,
+		Message: message,
+	})
+}
+
+func ErrorValidator(c *gin.Context, statusCode int, err error) {
+	var message string
+	castedErr, _ := err.(validator.ValidationErrors)
+	for _, validator := range castedErr {
+		switch validator.Tag() {
+		case "required" :
+			message = fmt.Sprintf("%s is required", validator.Field())
+		case "email" :
+			message = fmt.Sprintf("%s must be an email format", validator.Field())
+		case "min" :
+			message = fmt.Sprintf("%s must be greater than %s", validator.Field(), validator.Param())
+		}
+	}
+
+	c.JSON(statusCode, Response{
+		Code: "BAD_REQUEST",
 		Message: message,
 	})
 }
