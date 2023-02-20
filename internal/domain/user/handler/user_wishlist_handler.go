297c7fa6e0124c3eee545b9491715416d9a1d686
javin.javin <javin.javin@shopee.com>
2023-02-20 16:38:12 +0700

    feat(wishlist): handler unit tests

--- internal/domain/user/handler/user_wishlist_handler.go
+++ internal/domain/user/handler/user_wishlist_handler.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"errors"
 	"kedai/backend/be-kedai/internal/common/code"
+	errs "kedai/backend/be-kedai/internal/common/error"
 	"kedai/backend/be-kedai/internal/domain/user/dto"
 	"kedai/backend/be-kedai/internal/utils/response"
 
@@ -14,7 +16,7 @@ func (h *Handler) AddUserWishlist(c *gin.Context) {
 
 	err := c.ShouldBindJSON(&req)
 	if err != nil {
-		response.Error(c, 400, code.BAD_REQUEST, err.Error())
+		response.Error(c, 400, code.PRODUCT_CODE_IS_REQUIRED, errs.ErrProductCodeRequired.Error())
 		return
 	}
 	req.UserID = userId
@@ -22,10 +24,25 @@ func (h *Handler) AddUserWishlist(c *gin.Context) {
 	userWishlist, err := h.userWishlistService.AddUserWishlist(&req)
 
 	if err != nil {
+		if errors.Is(err, errs.ErrUserDoesNotExist) {
+			response.Error(c, 404, code.USER_NOT_REGISTERED, err.Error())
+			return
+		}
+
+		if errors.Is(err, errs.ErrProductDoesNotExist) {
+			response.Error(c, 404, code.PRODUCT_NOT_EXISTS, err.Error())
+			return
+		}
+
+		if errors.Is(err, errs.ErrProductInWishlist) {
+			response.Error(c, 400, code.PRODUCT_ALREADY_IN_WISHLIST, err.Error())
+			return
+		}
+
 		response.Error(c, 500, code.INTERNAL_SERVER_ERROR, err.Error())
 		return
 	}
 
-	response.Success(c, 201, code.CREATED, "Success add user wishlist", userWishlist)
+	response.Success(c, 201, code.CREATED, "ok", userWishlist)
 
 }
