4421e5c292f84dd6b11a5b39acea7cf1294f929d
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-02-20 10:37:01 +0700

    feat(user): user registration model

---
+++ internal/domain/user/model/user.go
@@ -0,0 +1,12 @@
+package entity
+
+import "gorm.io/gorm"
+
+type User struct {
+	ID       int          `json:"id"`
+	Email    string       `json:"email"`
+	Username string       `json:"username"`
+	Password string       `json:"-"`
+
+	gorm.Model `json:"-"`
+}
