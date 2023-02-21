cc36623260f6671de19c4b6ca446b21b679e8075
nugraha.andra <nugraha.andra@shopee.com>
2023-02-21 09:52:42 +0700

    refactor(get-cities): remove scope from dto

--- internal/domain/location/dto/city.go
+++ internal/domain/location/dto/city.go
@@ -1,9 +1,5 @@
 package dto
 
-import (
-	"gorm.io/gorm"
-)
-
 type GetCitiesRequest struct {
 	Limit      int    `form:"limit"`
 	Page       int    `form:"page"`
@@ -17,20 +13,6 @@ func (r *GetCitiesRequest) Validate() {
 	}
 	if r.Page < 1 {
 		r.Page = 1
-	}
-}
-
-func (r *GetCitiesRequest) Scope() func(db *gorm.DB) *gorm.DB {
-	return func(db *gorm.DB) *gorm.DB {
-		if r.ProvinceID != 0 {
-			db = db.Where("cities.province_id = ?", r.ProvinceID)
-		}
-		if r.Sort == "most_shops" {
-			db = db.Joins("left join user_addresses ua2 on cities.id = ua2.city_id and (select count(ua.id) from user_addresses ua inner join shops s on ua.id = s.address_id) > 0").
-				Group("cities.id").
-				Order("count(ua2.id) desc, cities.name asc")
-		}
-		return db
 	}
 }
 
