936714a000779cb75ff7e1e1f6d8a7a4227c4a24
nugraha.andra <nugraha.andra@shopee.com>
2023-02-22 11:32:07 +0700

    chore(register-wallet): add swagger register wallet

--- internal/domain/user/model/wallet.go
+++ internal/domain/user/model/wallet.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Wallet struct {
 	ID         int     `json:"id"`
-	UserID     int     `json:"user_id"`
+	UserID     int     `json:"userId"`
 	Pin        string  `json:"-"`
 	Balance    float64 `json:"balance"`
 	gorm.Model `json:"-"`
