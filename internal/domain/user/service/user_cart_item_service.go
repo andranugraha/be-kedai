149ec1f1c2c0b6fc32f682858e308fdfb6e44ba1
javin.javin <javin.javin@shopee.com>
2023-02-22 13:38:03 +0700

    feat(cart item): define logic for CreateCartItem service method

--- internal/domain/user/service/user_cart_item_service.go
+++ internal/domain/user/service/user_cart_item_service.go
@@ -1,6 +1,9 @@
 package service
 
 import (
+	"errors"
+	errs "kedai/backend/be-kedai/internal/common/error"
+	productService "kedai/backend/be-kedai/internal/domain/product/service"
 	"kedai/backend/be-kedai/internal/domain/user/dto"
 	"kedai/backend/be-kedai/internal/domain/user/model"
 	"kedai/backend/be-kedai/internal/domain/user/repository"
@@ -12,24 +15,62 @@ type UserCartItemService interface {
 
 type userCartItemServiceImpl struct {
 	cartItemRepository repository.UserCartItemRepository
+	skuService         productService.SkuService
+	userService        UserService
 }
 
 type UserCartItemSConfig struct {
 	CartItemRepository repository.UserCartItemRepository
+	SkuService         productService.SkuService
+	UserService        UserService
 }
 
 func NewUserCartItemService(cfg *UserCartItemSConfig) UserCartItemService {
 	return &userCartItemServiceImpl{
 		cartItemRepository: cfg.CartItemRepository,
+		skuService:         cfg.SkuService,
+		userService:        cfg.UserService,
 	}
 }
 
 func (s *userCartItemServiceImpl) CreateCartItem(cartItemReq *dto.UserCartItemRequest) (*model.UserCartItem, error) {
-	cartItem := cartItemReq.ToUserCartItem()
+	var result *model.UserCartItem
+	totalQuantity := cartItemReq.Quantity
 
-	result, err := s.cartItemRepository.CreateCartItem(cartItem)
+	// Check user
+	_, err := s.userService.GetByID(cartItemReq.UserId)
 	if err != nil {
 		return nil, err
+	}
+
+	// Check sku (product)
+	sku, err := s.skuService.GetByID(cartItemReq.SkuId)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check if cart item already exists
+	sameCartItem, err := s.cartItemRepository.GetCartItemByUserIdAndSkuId(cartItemReq.UserId, cartItemReq.SkuId)
+
+	if errors.Is(err, errs.ErrCartItemNotFound) {
+		cartItem := cartItemReq.ToUserCartItem()
+		result, err = s.cartItemRepository.CreateCartItem(cartItem)
+		if err != nil {
+			return nil, err
+		}
+
+		return result, err
+	}
+
+	if sameCartItem != nil {
+		totalQuantity += sameCartItem.Quantity
+		// Update Cart Quantity
+		return nil, nil
+	}
+
+	// Check sku quantity
+	if sku.Stock < totalQuantity {
+		return nil, errs.ErrSkuQuantityNotEnough
 	}
 
 	return result, nil
