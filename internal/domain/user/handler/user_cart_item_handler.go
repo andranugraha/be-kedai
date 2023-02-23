aaa79584039cea852d39a49349699030899d9074
javin.javin <javin.javin@shopee.com>
2023-02-23 15:45:57 +0700

    feat(cart): handler for add cart item

---
+++ internal/domain/user/handler/user_cart_item_handler.go
@@ -0,0 +1,51 @@
+package handler
+
+import (
+	"errors"
+	"kedai/backend/be-kedai/internal/common/code"
+	errs "kedai/backend/be-kedai/internal/common/error"
+	"kedai/backend/be-kedai/internal/domain/user/dto"
+	"kedai/backend/be-kedai/internal/utils/response"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) CreateCartItem(c *gin.Context) {
+	var cartItemReq dto.UserCartItemRequest
+	errBinding := c.ShouldBindJSON(&cartItemReq)
+	if errBinding != nil {
+		response.ErrorValidator(c, http.StatusBadRequest, errBinding)
+		return
+	}
+
+	userId := c.GetInt("userId")
+	cartItemReq.UserId = userId
+
+	cartItem, err := h.userCartItemService.CreateCartItem(&cartItemReq)
+	if err != nil {
+		if errors.Is(err, errs.ErrSkuNotFound) {
+			response.Error(c, http.StatusNotFound, code.PRODUCT_NOT_EXISTS, err.Error())
+			return
+		}
+
+		if errors.Is(err, errs.ErrSkuQuantityNotEnough) {
+			response.Error(c, http.StatusConflict, code.QUANTITY_NOT_ENOUGH, err.Error())
+			return
+		}
+
+		if errors.Is(err, errs.ErrProductDoesNotExist) {
+			response.Error(c, http.StatusNotFound, code.PRODUCT_NOT_EXISTS, err.Error())
+			return
+		}
+		if errors.Is(err, errs.ErrUserIsShopOwner) {
+			response.Error(c, http.StatusForbidden, code.FORBIDDEN, err.Error())
+			return
+		}
+
+		response.Error(c, http.StatusInternalServerError, code.INTERNAL_SERVER_ERROR, errs.ErrInternalServerError.Error())
+		return
+	}
+
+	response.Success(c, http.StatusCreated, code.CREATED, "create cart item succesful", cartItem)
+}
