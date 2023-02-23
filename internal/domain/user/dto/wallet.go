2e0e0ac027c300093aac6f21f777f19ea5ee9536
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-02-23 14:30:46 +0700

    feat(top-up): top-up request dto

--- internal/domain/user/dto/wallet.go
+++ internal/domain/user/dto/wallet.go
@@ -3,3 +3,7 @@ package dto
 type RegisterWalletRequest struct {
 	Pin string `json:"pin" binding:"required,numeric,len=6"`
 }
+
+type TopUpRequest struct {
+	Amount float64 `json:"amount" binding:"required,numeric,min=10000,max=20000000"`
+}
