a091df01097003e2a58f7973828234bec3b71850
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-02-24 16:21:57 +0700

    feat(shop): add slug and shop category

--- internal/domain/shop/model/shop.go
+++ internal/domain/shop/model/shop.go
@@ -7,12 +7,14 @@ import (
 )
 
 type Shop struct {
-	ID         int       `json:"id"`
-	Name       string    `json:"name"`
-	Rating     float64   `json:"rating"`
-	JoinedDate time.Time `json:"joinedDate"`
-	UserId     int       `json:"userId"`
-	AddressId  int       `json:"addressId"`
+	ID           int             `json:"id"`
+	Name         string          `json:"name"`
+	Rating       float64         `json:"rating"`
+	JoinedDate   time.Time       `json:"joinedDate"`
+	UserId       int             `json:"userId"`
+	AddressId    int             `json:"addressId"`
+	Slug         string          `json:"slug"`
+	ShopCategory []*ShopCategory `json:"shopCategory,omitempty"`
 
 	gorm.Model `json:"-"`
 }
