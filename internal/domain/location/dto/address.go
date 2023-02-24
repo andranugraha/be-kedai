00cf841da2dd0e7c0730f72bf15cdd309f8b6d6e
javin.javin <javin.javin@shopee.com>
2023-02-24 18:43:11 +0700

    chore: dto for add address

---
+++ internal/domain/location/dto/address.go
@@ -0,0 +1,23 @@
+package dto
+
+import "kedai/backend/be-kedai/internal/domain/location/model"
+
+type AddAddressRequest struct {
+	UserID        int    `json:"userId"`
+	Name          string `json:"name" binding:"required,max=20"`
+	PhoneNumber   string `json:"phoneNumber" binding:"required,numeric"`
+	Street        string `json:"street" binding:"required,max=100"`
+	Details       string `json:"details" binding:"max=50"`
+	SubdistrictID int    `json:"subdistrictId" binding:"required,numeric,min=1"`
+}
+
+func (r *AddAddressRequest) ToUserAddress() *model.UserAddress {
+	return &model.UserAddress{
+		UserID:        r.UserID,
+		Name:          r.Name,
+		PhoneNumber:   r.PhoneNumber,
+		Street:        r.Street,
+		Details:       r.Details,
+		SubdistrictID: r.SubdistrictID,
+	}
+}
