da1af37fe94fb9e975ac271647284f5c45369b5a
Izharul Haq <izharul.haq@shopee.com>
2023-02-24 10:16:39 +0700

    feat(update-username): add username length constraint

--- internal/domain/user/dto/user.go
+++ internal/domain/user/dto/user.go
@@ -22,7 +22,7 @@ type UpdateEmailRequest struct {
 }
 
 type UpdateUsernameRequest struct {
-	Username string `json:"username" binding:"required,alphanum"`
+	Username string `json:"username" binding:"required,min=6,max=30"`
 }
 
 type UpdateUsernameResponse struct {
