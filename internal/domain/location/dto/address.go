912e100e6f1e519a32b5ab25d04f40557e270667
javin.javin <javin.javin@shopee.com>
2023-02-24 21:17:20 +0700

    chore: add field isDefault

--- internal/domain/location/dto/address.go
+++ internal/domain/location/dto/address.go
@@ -9,6 +9,7 @@ type AddAddressRequest struct {
 	Street        string `json:"street" binding:"required,max=100"`
 	Details       string `json:"details" binding:"max=50"`
 	SubdistrictID int    `json:"subdistrictId" binding:"required,numeric,min=1"`
+	IsDefault     bool   `json:"isDefault" binding:"required"`
 }
 
 func (r *AddAddressRequest) ToUserAddress() *model.UserAddress {
