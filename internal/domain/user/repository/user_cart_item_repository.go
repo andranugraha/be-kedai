372b9fe4e31dd20a490cc524694452a76f1f4190
javin.javin <javin.javin@shopee.com>
2023-02-24 15:52:31 +0700

    feat(cart): repo for get all cart

--- internal/domain/user/repository/user_cart_item_repository.go
+++ internal/domain/user/repository/user_cart_item_repository.go
@@ -3,13 +3,16 @@ package repository
 import (
 	"errors"
 	errs "kedai/backend/be-kedai/internal/common/error"
+	"kedai/backend/be-kedai/internal/domain/user/dto"
 	"kedai/backend/be-kedai/internal/domain/user/model"
+	"math"
 
 	"gorm.io/gorm"
 )
 
 type UserCartItemRepository interface {
 	CreateCartItem(cartItem *model.CartItem) (*model.CartItem, error)
+	GetAllCartItem(*dto.GetCartItemsRequest) (cartItems []*model.CartItem, totalRows int64, totalPages int, err error)
 	GetCartItemByUserIdAndSkuId(userId int, skuId int) (*model.CartItem, error)
 	UpdateCartItem(cartItem *model.CartItem) (*model.CartItem, error)
 }
@@ -64,4 +67,31 @@ func (r *userCartItemRepository) UpdateCartItem(cartItem *model.CartItem) (*mode
 	}
 
 	return cartItem, nil
+}
+
+func (r *userCartItemRepository) GetAllCartItem(req *dto.GetCartItemsRequest) (cartItems []*model.CartItem, totalRows int64, totalPages int, err error) {
+
+	db := r.db.Where("cart_items.user_id = ?", req.UserId).
+		Joins("left join skus s on cart_items.sku_id = s.id").
+		Joins("left join products p on s.product_id = p.id").
+		Joins("left join shops sh on p.shop_id = sh.id").
+		Group("sh.id, cart_items.id").
+		Order("cart_items.created_at").
+		Preload("Sku.Product.Shop").
+		Preload("Sku.Variants.Group").
+		Preload("Sku.Promotion")
+
+	db.Model(&model.CartItem{}).Count(&totalRows)
+
+	totalPages = 1
+	if req.Limit > 0 {
+		totalPages = int(math.Ceil(float64(totalRows) / float64(req.Limit)))
+	}
+
+	err = db.Limit(req.Limit).Offset(req.Offset()).Find(&cartItems).Error
+	if err != nil {
+		return
+	}
+
+	return
 }
