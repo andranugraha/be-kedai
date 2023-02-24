8e3765f6f9f9e07982984155251499ca7a88d486
javin.javin <javin.javin@shopee.com>
2023-02-24 15:50:24 +0700

    feat(cart): add route to get cart items

--- internal/server/router.go
+++ internal/server/router.go
@@ -54,6 +54,7 @@ func NewRouter(cfg *RouterConfig) *gin.Engine {
 				carts := userAuthenticated.Group("/carts")
 				{
 					carts.POST("", cfg.UserHandler.CreateCartItem)
+					carts.GET("", cfg.UserHandler.GetAllCartItem)
 				}
 			}
 		}
