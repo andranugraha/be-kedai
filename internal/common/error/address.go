c50ce9a14620907664dba832cfd681c906e67dda
javin.javin <javin.javin@shopee.com>
2023-02-27 16:42:38 +0700

    chore: error address not found

--- internal/common/error/address.go
+++ internal/common/error/address.go
@@ -3,5 +3,6 @@ package error
 import "errors"
 
 var (
-	ErrMaxAddress = errors.New("max address reached")
+	ErrMaxAddress      = errors.New("max address reached")
+	ErrAddressNotFound = errors.New("address not found")
 )
