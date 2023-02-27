afc0a0e89f593ebfcbb6dea69b9cba8b736e95ff
nugraha.andra <nugraha.andra@shopee.com>
2023-02-27 17:22:37 +0700

    refactor(sealabs-pay): remove json tag on user id and adjust response validator

--- internal/domain/user/dto/sealabs_pay.go
+++ internal/domain/user/dto/sealabs_pay.go
@@ -4,5 +4,5 @@ type CreateSealabsPayRequest struct {
 	CardNumber string `json:"cardNumber" binding:"required,len=16,numeric"`
 	CardName   string `json:"cardName" binding:"required"`
 	ExpiryDate string `json:"expiryDate" binding:"required,datetime=01/06"`
-	UserID     int    `json:"userId"`
+	UserID     int
 }
