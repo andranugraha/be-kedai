5187dde62f472ac396f1f5bd96daf3a0f81397c2
javin.javin <javin.javin@shopee.com>
2023-02-27 14:06:26 +0700

    chore: change validation requirement

--- internal/domain/location/dto/address.go
+++ internal/domain/location/dto/address.go
@@ -3,11 +3,11 @@ package dto
 import "kedai/backend/be-kedai/internal/domain/location/model"
 
 type AddAddressRequest struct {
-	UserID        int    `json:"userId"`
-	Name          string `json:"name" binding:"required,max=20"`
+	UserID        int
+	Name          string `json:"name" binding:"required,max=30"`
 	PhoneNumber   string `json:"phoneNumber" binding:"required,numeric"`
-	Street        string `json:"street" binding:"required,max=100"`
-	Details       string `json:"details" binding:"max=50"`
+	Street        string `json:"street" binding:"required,max=200"`
+	Details       string `json:"details" binding:"max=30"`
 	SubdistrictID int    `json:"subdistrictId" binding:"required,numeric,min=1"`
 	IsDefault     *bool  `json:"isDefault" binding:"required"`
 }
