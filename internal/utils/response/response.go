f0ab2cec94baf792f29539e1cc909f32d224f199
Izharul Haq <izharul.haq@shopee.com>
2023-02-27 10:26:06 +0700

    fix(update-profile): fix update profile error message

--- internal/utils/response/response.go
+++ internal/utils/response/response.go
@@ -2,6 +2,7 @@ package response
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
@@ -49,6 +50,21 @@ func ErrorValidator(c *gin.Context, statusCode int, err error) {
 			message = fmt.Sprintf("%s must be alphanumeric", validator.Field())
 		case "datetime":
 			message = fmt.Sprintf("%s must be YYYY-MM-DD", validator.Field())
+		case "url":
+			message = fmt.Sprintf("%s must be a URL", validator.Field())
+		case "oneof":
+			vals := validator.Param()
+			valueList := strings.Split(vals, " ")
+			valMessage := ""
+			for i, v := range valueList {
+				if i == len(valueList)-1 {
+					valMessage += fmt.Sprintf("or %s", v)
+					continue
+				}
+
+				valMessage += fmt.Sprintf("%s, ", v)
+			}
+			message = fmt.Sprintf("%s must be either %s", validator.Field(), valMessage)
 		}
 	}
 
