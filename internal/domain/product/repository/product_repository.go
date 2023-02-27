ea745b67655cb5bd307df9884682f81c21839187
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-02-27 15:33:16 +0700

    feat(recommendation): product recommendation repo

--- internal/domain/product/repository/product_repository.go
+++ internal/domain/product/repository/product_repository.go
@@ -11,6 +11,7 @@ import (
 type ProductRepository interface {
 	GetByID(ID int) (*model.Product, error)
 	GetByCode(Code string) (*model.Product, error)
+	GetRecommendation(categoryId int) ([]*model.Product, error)
 }
 
 type productRepositoryImpl struct {
@@ -55,4 +56,27 @@ func (r *productRepositoryImpl) GetByCode(Code string) (*model.Product, error) {
 	}
 
 	return &product, nil
+}
+
+func (r *productRepositoryImpl) GetRecommendation(categoryId int) ([]*model.Product, error) {
+	var (
+		products []*model.Product
+		limit    = 5
+		isActive = true
+	)
+
+	db := r.db.Select(`products.*, min(s.price) as min_price, max(s.price) as max_price,
+	max(case when pp.type = 'nominal' then ROUND(cast(pp.amount / s.price * 100 as numeric), 2) else pp.amount end) as promotion_percent,
+	(select url from product_medias pm where pm.product_id = products.id limit 1) as image_url`).
+		Joins("join skus s on s.product_id = products.id").
+		Joins("join shops sh on sh.id = products.shop_id").
+		Joins("left join product_promotions pp on pp.sku_id = s.id and (select count(id) from shop_promotions sp where pp.promotion_id = sp.id and now() between sp.start_period and sp.end_period) > 0").
+		Group("products.id")
+
+	err := db.Where("products.category_id = ? and products.is_active = ?", categoryId, isActive).Limit(limit).Order("products.sold desc, products.rating desc").Find(&products).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return products, nil
 }
