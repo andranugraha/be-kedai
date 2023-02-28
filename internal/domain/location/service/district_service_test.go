f713d0ccdd57fb754e3014c2f3564249a9cf5091
javin.javin <javin.javin@shopee.com>
2023-02-28 14:02:26 +0700

    feat(district): get districts tests

--- internal/domain/location/service/district_service_test.go
+++ internal/domain/location/service/district_service_test.go
@@ -1,6 +1,7 @@
 package service_test
 
 import (
+	"kedai/backend/be-kedai/internal/domain/location/dto"
 	"kedai/backend/be-kedai/internal/domain/location/model"
 	"kedai/backend/be-kedai/internal/domain/location/service"
 	"kedai/backend/be-kedai/mocks"
@@ -8,6 +9,68 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestGetDistricts(t *testing.T) {
+	type input struct {
+		data        dto.GetDistrictsRequest
+		err         error
+		beforeTests func(mockDistrictRepo *mocks.DistrictRepository)
+	}
+	type expected struct {
+		data []*model.District
+		err  error
+	}
+
+	cases := []struct {
+		description string
+		input
+		expected
+	}{
+		{
+			description: "should return districts and error",
+			input: input{
+				data: dto.GetDistrictsRequest{
+					CityID: 1,
+				},
+				err: nil,
+				beforeTests: func(mockDistrictRepo *mocks.DistrictRepository) {
+					mockDistrictRepo.On("GetAll", dto.GetDistrictsRequest{
+						CityID: 1,
+					}).Return([]*model.District{
+						{
+							ID: 1,
+						},
+					}, nil)
+				},
+			},
+			expected: expected{
+				data: []*model.District{
+					{
+						ID: 1,
+					},
+				},
+				err: nil,
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.description, func(t *testing.T) {
+			mockDistrictRepo := mocks.NewDistrictRepository(t)
+			c.beforeTests(mockDistrictRepo)
+
+			districtService := service.NewDistrictService(&service.DistrictSConfig{
+				DistrictRepo: mockDistrictRepo,
+			})
+
+			data, err := districtService.GetDistricts(c.input.data)
+
+			assert.Equal(t, c.expected.err, err)
+			assert.Equal(t, c.expected.data, data)
+		})
+	}
+
+}
 
 func TestGetDistrictByID(t *testing.T) {
 	type input struct {
