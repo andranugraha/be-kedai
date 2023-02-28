ac09580e76c8305a34ba55148663d54d13cc37b9
javin.javin <javin.javin@shopee.com>
2023-02-28 10:59:20 +0700

    feat(user auth): handler method sign up with google

--- internal/domain/user/handler/user_handler.go
+++ internal/domain/user/handler/user_handler.go
@@ -80,6 +80,44 @@ func (h *Handler) UserLogin(c *gin.Context) {
 	response.Success(c, http.StatusOK, code.OK, "ok", token)
 }
 
+func (h *Handler) UserRegistrationWithGoogle(c *gin.Context) {
+	var newUser dto.UserRegistrationWithGoogleRequest
+	errBinding := c.ShouldBindJSON(&newUser)
+	if errBinding != nil {
+		response.ErrorValidator(c, http.StatusBadRequest, errBinding)
+		return
+	}
+
+	user, err := h.userService.SignUpWithGoogle(&newUser)
+	if err != nil {
+		if errors.Is(err, errs.ErrUserAlreadyExist) {
+			response.Error(c, http.StatusConflict, code.EMAIL_ALREADY_REGISTERED, errs.ErrUserAlreadyExist.Error())
+			return
+		}
+		if errors.Is(err, errs.ErrUsernameUsed) {
+			response.Error(c, http.StatusConflict, code.USERNAME_ALREADY_REGISTERED, err.Error())
+			return
+		}
+		if errors.Is(err, errs.ErrInvalidPasswordPattern) {
+			response.Error(c, http.StatusUnprocessableEntity, code.INVALID_PASSWORD_PATTERN, err.Error())
+			return
+		}
+		if errors.Is(err, errs.ErrContainEmail) {
+			response.Error(c, http.StatusUnprocessableEntity, code.PASSWORD_CONTAIN_EMAIL, err.Error())
+			return
+		}
+		if errors.Is(err, errs.ErrUnauthorized) {
+			response.Error(c, http.StatusUnauthorized, code.UNAUTHORIZED, err.Error())
+			return
+		}
+
+		response.Error(c, http.StatusInternalServerError, code.INTERNAL_SERVER_ERROR, err.Error())
+		return
+	}
+
+	response.Success(c, http.StatusCreated, code.CREATED, "Sign up with google successful", user)
+}
+
 func (h *Handler) UserLoginWithGoogle(c *gin.Context) {
 	var newLogin dto.UserLoginWithGoogleRequest
 	errBinding := c.ShouldBindJSON(&newLogin)
