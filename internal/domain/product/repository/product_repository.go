bb917f11b4b6d5cce0da04d10004091b64039260
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-02-28 11:05:52 +0700

    feat(recommendation): add default sku

--- internal/domain/product/repository/product_repository.go
+++ internal/domain/product/repository/product_repository.go
@@ -68,7 +68,8 @@ func (r *productRepositoryImpl) GetRecommendationByCategory(productId int, categ
 
 	db := r.db.Select(`products.*, min(s.price) as min_price, max(s.price) as max_price,
 	max(case when pp.type = 'nominal' then ROUND(cast(pp.amount / s.price * 100 as numeric), 2) else pp.amount end) as promotion_percent,
-	(select url from product_medias pm where pm.product_id = products.id limit 1) as image_url`).
+	(select url from product_medias pm where pm.product_id = products.id limit 1) as image_url,
+	(select id from skus where products.id = skus.product_id limit 1) as default_sku_id`).
 		Joins("join skus s on s.product_id = products.id").
 		Joins("join shops sh on sh.id = products.shop_id").
 		Joins("left join product_promotions pp on pp.sku_id = s.id and (select count(id) from shop_promotions sp where pp.promotion_id = sp.id and now() between sp.start_period and sp.end_period) > 0").
