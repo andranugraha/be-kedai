451d74b5d59a9f73f417c2aef0dadfc6ad1aacef
Izharul Haq <izharul.haq@shopee.com>
2023-02-28 16:15:58 +0700

    test(update-cart): implement update cart handler unit test

--- internal/domain/user/handler/user_cart_item_handler_test.go
+++ internal/domain/user/handler/user_cart_item_handler_test.go
@@ -2,6 +2,7 @@ package handler_test
 
 import (
 	"encoding/json"
+	"errors"
 	"kedai/backend/be-kedai/internal/common/code"
 	commonDto "kedai/backend/be-kedai/internal/common/dto"
 	errs "kedai/backend/be-kedai/internal/common/error"
@@ -295,4 +296,200 @@ func TestGetAllCartItem(t *testing.T) {
 		})
 	}
 
+}
+
+func TestUpdateCartItem(t *testing.T) {
+	type input struct {
+		userID     int
+		skuID      string
+		request    *dto.UpdateCartItemRequest
+		beforeTest func(*mocks.UserCartItemService)
+	}
+	type expected struct {
+		statusCode int
+		response   response.Response
+	}
+
+	tests := []struct {
+		description string
+		input
+		expected
+	}{
+		{
+			description: "should return error with status code 400 when sku ID is invalid",
+			input: input{
+				userID: 1,
+				skuID:  "-1",
+				request: &dto.UpdateCartItemRequest{
+					SkuID: 2,
+				},
+				beforeTest: func(ucis *mocks.UserCartItemService) {},
+			},
+			expected: expected{
+				statusCode: http.StatusBadRequest,
+				response: response.Response{
+					Code:    code.BAD_REQUEST,
+					Message: "sku ID must be a number and greater than or equal 1",
+				},
+			},
+		},
+		{
+			description: "should return error with status code 400 when request body is empty",
+			input: input{
+				userID:     1,
+				skuID:      "2",
+				request:    &dto.UpdateCartItemRequest{},
+				beforeTest: func(ucis *mocks.UserCartItemService) {},
+			},
+			expected: expected{
+				statusCode: http.StatusBadRequest,
+				response: response.Response{
+					Code:    code.BAD_REQUEST,
+					Message: "Notes is required",
+				},
+			},
+		},
+		{
+			description: "should return error with status code 400 when given invalid request body",
+			input: input{
+				userID: 1,
+				skuID:  "2",
+				request: &dto.UpdateCartItemRequest{
+					Notes: "a veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery long notes",
+				},
+				beforeTest: func(ucis *mocks.UserCartItemService) {},
+			},
+			expected: expected{
+				statusCode: http.StatusBadRequest,
+				response: response.Response{
+					Code:    code.BAD_REQUEST,
+					Message: "Notes must be shorter than 50",
+				},
+			},
+		},
+		{
+			description: "should return error with status code 404 when product does not exist or inactive",
+			input: input{
+				userID: 1,
+				skuID:  "2",
+				request: &dto.UpdateCartItemRequest{
+					Quantity: 3,
+					Notes:    "test",
+				},
+				beforeTest: func(ucis *mocks.UserCartItemService) {
+					ucis.On("UpdateCartItem", 1, &dto.UpdateCartItemRequest{
+						SkuID:    2,
+						Quantity: 3,
+						Notes:    "test",
+					}).Return(nil, errs.ErrProductDoesNotExist)
+				},
+			},
+			expected: expected{
+				statusCode: http.StatusNotFound,
+				response: response.Response{
+					Code:    code.PRODUCT_NOT_EXISTS,
+					Message: errs.ErrProductDoesNotExist.Error(),
+				},
+			},
+		},
+		{
+			description: "should return error with status code 409 when product does not exist or inactive",
+			input: input{
+				userID: 1,
+				skuID:  "2",
+				request: &dto.UpdateCartItemRequest{
+					Quantity: 3,
+					Notes:    "test",
+				},
+				beforeTest: func(ucis *mocks.UserCartItemService) {
+					ucis.On("UpdateCartItem", 1, &dto.UpdateCartItemRequest{
+						SkuID:    2,
+						Quantity: 3,
+						Notes:    "test",
+					}).Return(nil, errs.ErrProductQuantityNotEnough)
+				},
+			},
+			expected: expected{
+				statusCode: http.StatusConflict,
+				response: response.Response{
+					Code:    code.QUANTITY_NOT_ENOUGH,
+					Message: errs.ErrProductQuantityNotEnough.Error(),
+				},
+			},
+		},
+		{
+			description: "should return error with status code 500 when failed to update cart item",
+			input: input{
+				userID: 1,
+				skuID:  "2",
+				request: &dto.UpdateCartItemRequest{
+					Quantity: 3,
+					Notes:    "test",
+				},
+				beforeTest: func(ucis *mocks.UserCartItemService) {
+					ucis.On("UpdateCartItem", 1, &dto.UpdateCartItemRequest{
+						SkuID:    2,
+						Quantity: 3,
+						Notes:    "test",
+					}).Return(nil, errors.New("failed to update cart item"))
+				},
+			},
+			expected: expected{
+				statusCode: http.StatusInternalServerError,
+				response: response.Response{
+					Code:    code.INTERNAL_SERVER_ERROR,
+					Message: errs.ErrInternalServerError.Error(),
+				},
+			},
+		},
+		{
+			description: "should return error with status code 200 when update cart item succeed",
+			input: input{
+				userID: 1,
+				skuID:  "2",
+				request: &dto.UpdateCartItemRequest{
+					SkuID:    2,
+					Quantity: 3,
+					Notes:    "test",
+				},
+				beforeTest: func(ucis *mocks.UserCartItemService) {
+					ucis.On("UpdateCartItem", 1, &dto.UpdateCartItemRequest{
+						SkuID:    2,
+						Quantity: 3,
+						Notes:    "test",
+					}).Return(&model.CartItem{ID: 2, SkuId: 2, Quantity: 3, Notes: "test"}, nil)
+				},
+			},
+			expected: expected{
+				statusCode: http.StatusOK,
+				response: response.Response{
+					Code:    code.UPDATED,
+					Message: "update cart item succesful",
+					Data:    &model.CartItem{ID: 2, SkuId: 2, Quantity: 3, Notes: "test"},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			expectedRes, _ := json.Marshal(tc.expected.response)
+			cartItemService := mocks.NewUserCartItemService(t)
+			tc.beforeTest(cartItemService)
+			rec := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(rec)
+			c.Set("userId", tc.input.userID)
+			c.AddParam("skuId", tc.input.skuID)
+			payload := test.MakeRequestBody(tc.input.request)
+			c.Request, _ = http.NewRequest(http.MethodPut, "v1/users/carts", payload)
+			handler := handler.New(&handler.HandlerConfig{
+				UserCartItemService: cartItemService,
+			})
+
+			handler.UpdateCartItem(c)
+
+			assert.Equal(t, tc.expected.statusCode, rec.Code)
+			assert.Equal(t, string(expectedRes), rec.Body.String())
+		})
+	}
 }
