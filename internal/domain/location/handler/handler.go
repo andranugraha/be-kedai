25fc344a83065b19a901cc88510abf69b4e82bf5
javin.javin <javin.javin@shopee.com>
2023-02-28 14:25:24 +0700

    feat(subistrict): get subdistricts handler

--- internal/domain/location/handler/handler.go
+++ internal/domain/location/handler/handler.go
@@ -3,21 +3,24 @@ package handler
 import "kedai/backend/be-kedai/internal/domain/location/service"
 
 type Handler struct {
-	cityService     service.CityService
-	provinceService service.ProvinceService
-	districtService service.DistrictService
+	cityService        service.CityService
+	provinceService    service.ProvinceService
+	districtService    service.DistrictService
+	subdistrictService service.SubdistrictService
 }
 
 type Config struct {
-	CityService     service.CityService
-	ProvinceService service.ProvinceService
-	DistrictService service.DistrictService
+	CityService        service.CityService
+	ProvinceService    service.ProvinceService
+	DistrictService    service.DistrictService
+	SubdistrictService service.SubdistrictService
 }
 
 func New(cfg *Config) *Handler {
 	return &Handler{
-		cityService:     cfg.CityService,
-		provinceService: cfg.ProvinceService,
-		districtService: cfg.DistrictService,
+		cityService:        cfg.CityService,
+		provinceService:    cfg.ProvinceService,
+		districtService:    cfg.DistrictService,
+		subdistrictService: cfg.SubdistrictService,
 	}
 }
