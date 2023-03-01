6d40cb49d932e554bde0add339064c66a326fefb
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-03-01 17:28:19 +0700

    feat(search): product filter repository

--- internal/domain/product/repository/product_repository.go
+++ internal/domain/product/repository/product_repository.go
@@ -2,9 +2,11 @@ package repository
 
 import (
 	"errors"
+	"kedai/backend/be-kedai/internal/common/constant"
 	errs "kedai/backend/be-kedai/internal/common/error"
 	"kedai/backend/be-kedai/internal/domain/product/dto"
 	model "kedai/backend/be-kedai/internal/domain/product/model"
+	"math"
 
 	"gorm.io/gorm"
 )
@@ -13,6 +15,7 @@ type ProductRepository interface {
 	GetByID(ID int) (*model.Product, error)
 	GetByCode(Code string) (*model.Product, error)
 	GetRecommendationByCategory(productId int, categoryId int) ([]*dto.ProductResponse, error)
+	ProductSearchFiltering(req dto.ProductSearchFilterRequest) ([]*dto.ProductResponse, int64, int, error)
 }
 
 type productRepositoryImpl struct {
@@ -82,4 +85,74 @@ func (r *productRepositoryImpl) GetRecommendationByCategory(productId int, categ
 	}
 
 	return products, nil
+}
+
+func (r *productRepositoryImpl) ProductSearchFiltering(req dto.ProductSearchFilterRequest) ([]*dto.ProductResponse, int64, int, error) {
+	var (
+		productList []*dto.ProductResponse
+		totalRows   int64
+		totalPages  int
+		active      = true
+	)
+
+	db := r.db.Select(`products.*, min(s.price) as min_price, max(s.price) as max_price, 
+	concat(c.name, ', ', p.name) as address, 
+	max(case when pp.type = 'nominal' then pp.amount / s.price else pp.amount end) as promotion_percent, 
+	(select url from product_medias pm where products.id = pm.product_id limit 1) as image_url`).
+		Joins("join skus s ON s.product_id = products.id").
+		Joins("join shops sh ON sh.id = products.shop_id").
+		Joins("join user_addresses ua ON ua.id = sh.address_id").
+		Joins("join cities c ON c.id = ua.city_id").
+		Joins("join provinces p ON p.id = c.province_id").
+		Joins("left join product_promotions pp ON pp.sku_id = s.id and (select count(id) from shop_promotions sp where pp.promotion_id = sp.id and now() between sp.start_period and sp.end_period) > 0").
+		Group("products.id, c.name, p.name")
+
+	db = db.Where("products.is_active = ?", active).Where("products.name ILIKE ?", "%"+req.Keyword+"%")
+
+	if req.CategoryId > 0 {
+		db = db.Where("products.category_id = ?", req.CategoryId)
+	}
+
+	if req.MinRating > 0 {
+		db = db.Where("products.rating >= ?", req.MinRating)
+	}
+
+	if len(req.CityIds) > 0 {
+		db = db.Where("ua.city_id in (?)", req.CityIds)
+	}
+
+	if req.MinPrice > 0 || req.MaxPrice > 0 {
+		if req.MinPrice > 0 {
+			db = db.Where("s.id = (select id from skus where product_id = products.id and skus.price >= ? limit 1)", req.MinPrice)
+		}
+
+		if req.MaxPrice > 0 {
+			db = db.Where("s.id = (select id from skus where product_id = products.id and skus.price <= ? limit 1)", req.MaxPrice)
+		}
+	}
+
+	switch req.Sort {
+	case constant.SortByRecommended:
+		db = db.Order("products.rating desc, products.sold desc")
+	case constant.SortByLatest:
+		db = db.Order("products.created_at desc")
+	case constant.SortByTopSales:
+		db = db.Order("products.sold desc")
+	case constant.SortByPriceLow:
+		db = db.Where("s.id = (select id from skus where product_id = products.id order by price asc limit 1)").Order("s.price asc")
+	case constant.SortByPriceHigh:
+		db = db.Where("s.id = (select id from skus where product_id = products.id order by price asc limit 1)").Order("s.price desc")
+	default:
+		db = db.Order("products.created_at desc")
+	}
+
+	db.Model(&model.Product{}).Count(&totalRows)
+	totalPages = int(math.Ceil(float64(totalRows) / float64(req.Limit)))
+
+	err := db.Model(&model.Product{}).Limit(req.Limit).Offset(req.Offset()).Find(&productList).Error
+	if err != nil {
+		return nil, 0, 0, err
+	}
+
+	return productList, totalRows, totalPages, nil
 }
