17b48d9c3d1361b6048a4b1faa5f5b2393193f0a
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-03-01 17:28:46 +0700

    feat(search): product filter service

--- internal/domain/product/service/product_service.go
+++ internal/domain/product/service/product_service.go
@@ -1,15 +1,18 @@
 package service
 
 import (
+	commonDto "kedai/backend/be-kedai/internal/common/dto"
 	"kedai/backend/be-kedai/internal/domain/product/dto"
 	"kedai/backend/be-kedai/internal/domain/product/model"
 	"kedai/backend/be-kedai/internal/domain/product/repository"
+	"strings"
 )
 
 type ProductService interface {
 	GetByID(id int) (*model.Product, error)
 	GetByCode(code string) (*model.Product, error)
 	GetRecommendationByCategory(productId int, categoryId int) ([]*dto.ProductResponse, error)
+	ProductSearchFiltering(req dto.ProductSearchFilterRequest) (*commonDto.PaginationResponse, error)
 }
 
 type productServiceImpl struct {
@@ -36,4 +39,32 @@ func (s *productServiceImpl) GetByCode(code string) (*model.Product, error) {
 
 func (s *productServiceImpl) GetRecommendationByCategory(productId int, categoryId int) ([]*dto.ProductResponse, error) {
 	return s.productRepository.GetRecommendationByCategory(productId, categoryId)
+}
+
+func (s *productServiceImpl) ProductSearchFiltering(req dto.ProductSearchFilterRequest) (*commonDto.PaginationResponse, error) {
+	validateKeyword := strings.Trim(req.Keyword, " ")
+	if validateKeyword == "" {
+		return &commonDto.PaginationResponse{
+			Data: []*dto.ProductResponse{},
+			Limit: req.Limit,
+			Page: req.Page,
+			TotalRows: 0,
+			TotalPages: 0,
+		}, nil
+	}
+	
+	res, rows, pages, err := s.productRepository.ProductSearchFiltering(req)
+	if err != nil {
+		return nil, err
+	}
+
+	response := &commonDto.PaginationResponse{
+		Data:       res,
+		Limit:      req.Limit,
+		Page:       req.Page,
+		TotalRows:  rows,
+		TotalPages: pages,
+	}
+
+	return response, nil
 }
