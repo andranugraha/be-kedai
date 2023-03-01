14a782b01b033cf6c161fab5614724e6e620ded8
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-03-01 11:52:51 +0700

    refactor(recommendation): remove join shop in query

--- internal/domain/product/repository/product_repository.go
+++ internal/domain/product/repository/product_repository.go
@@ -70,7 +70,6 @@ func (r *productRepositoryImpl) GetRecommendationByCategory(productId int, categ
 	max(case when pp.type = 'nominal' then pp.amount / s.price else pp.amount end) as promotion_percent,
 	(select url from product_medias pm where pm.product_id = products.id limit 1) as image_url`).
 		Joins("join skus s on s.product_id = products.id").
-		Joins("join shops sh on sh.id = products.shop_id").
 		Joins("left join product_promotions pp on pp.sku_id = s.id and (select count(id) from shop_promotions sp where pp.promotion_id = sp.id and now() between sp.start_period and sp.end_period) > 0").
 		Group("products.id")
 
