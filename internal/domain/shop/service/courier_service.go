eeb07e243e4a5bd8e9d97dcc213e3dd7d7ff7b58
Izharul Haq <izharul.haq@shopee.com>
2023-03-01 16:02:30 +0700

    feat(get-product): implement get couriers by shop id service

---
+++ internal/domain/shop/service/courier_service.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"kedai/backend/be-kedai/internal/domain/shop/model"
+	"kedai/backend/be-kedai/internal/domain/shop/repository"
+)
+
+type CourierService interface {
+	GetCouriersByShopID(shopID int) ([]*model.Courier, error)
+}
+
+type courierServiceImpl struct {
+	courierRepository repository.CourierRepository
+	shopService       ShopService
+}
+
+type CourierSConfig struct {
+	CourierRepository repository.CourierRepository
+	ShopService       ShopService
+}
+
+func NewCourierService(cfg *CourierSConfig) CourierService {
+	return &courierServiceImpl{
+		courierRepository: cfg.CourierRepository,
+		shopService:       cfg.ShopService,
+	}
+}
+
+func (s *courierServiceImpl) GetCouriersByShopID(shopID int) ([]*model.Courier, error) {
+	return s.courierRepository.GetByShopID(shopID)
+}
