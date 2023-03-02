131a1225c0e6e9ac1aa9824a4a5988f6f04b248f
javin.javin <javin.javin@shopee.com>
2023-03-02 17:30:44 +0700

    chore: handler errors

--- internal/domain/order/handler/transaction_review_handler.go
+++ internal/domain/order/handler/transaction_review_handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"errors"
 	"kedai/backend/be-kedai/internal/common/code"
 	"kedai/backend/be-kedai/internal/domain/order/dto"
 	"kedai/backend/be-kedai/internal/utils/response"
@@ -23,6 +24,19 @@ func (h *Handler) AddTransactionReview(c *gin.Context) {
 
 	err := h.transactionReviewService.Create(req)
 	if err != nil {
+		if errors.Is(err, commonErr.ErrTransactionReviewAlreadyExist) {
+			response.Error(c, http.StatusConflict, code.TRANSACTION_REVIEW_ALREADY_EXIST, err.Error())
+			return
+		}
+		if errors.Is(err, commonErr.ErrTransactionNotFound) {
+			response.Error(c, http.StatusNotFound, code.TRANSACTION_NOT_FOUND, err.Error())
+			return
+		}
+		if errors.Is(err, commonErr.ErrInvoiceNotCompleted) {
+			response.Error(c, http.StatusConflict, code.INVOICE_NOT_COMPLETED, err.Error())
+			return
+		}
+
 		response.Error(c, http.StatusInternalServerError, code.INTERNAL_SERVER_ERROR, commonErr.ErrInternalServerError.Error())
 		return
 	}
