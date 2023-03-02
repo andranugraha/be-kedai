1400737f40f23a1ccd80d1e6808b10358ecbb028
javin.javin <javin.javin@shopee.com>
2023-03-02 17:30:30 +0700

    feat(review): additional validation checking

--- internal/domain/order/service/transaction_review_service.go
+++ internal/domain/order/service/transaction_review_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	commonErr "kedai/backend/be-kedai/internal/common/error"
 	"kedai/backend/be-kedai/internal/domain/order/dto"
 	"kedai/backend/be-kedai/internal/domain/order/repository"
 )
@@ -11,22 +12,45 @@ type TransactionReviewService interface {
 
 type transactionReviewServiceImpl struct {
 	transactionReviewRepo repository.TransactionReviewRepository
+	transactionService    TransactionService
+	invoicePerShopService InvoicePerShopService
 }
 
 type TransactionReviewSConfig struct {
 	TransactionReviewRepo repository.TransactionReviewRepository
+	TransactionService    TransactionService
+	InvoicePerShopService InvoicePerShopService
 }
 
 func NewTransactionReviewService(config *TransactionReviewSConfig) TransactionReviewService {
 	return &transactionReviewServiceImpl{
 		transactionReviewRepo: config.TransactionReviewRepo,
+		transactionService:    config.TransactionService,
+		invoicePerShopService: config.InvoicePerShopService,
 	}
 }
 
 func (s *transactionReviewServiceImpl) Create(req dto.TransactionReviewRequest) error {
-	transactionReview := req.ToModel()
+	transaction, err := s.transactionService.GetByID(req.TransactionId)
+	if err != nil {
+		return err
+	}
 
-	err := s.transactionReviewRepo.Create(transactionReview)
+	if transaction.UserID != req.UserId {
+		return commonErr.ErrTransactionNotFound
+	}
+
+	invoicePerShop, err := s.invoicePerShopService.GetByID(transaction.InvoiceID)
+	if err != nil {
+		return err
+	}
+
+	if invoicePerShop.Status != "completed" {
+		return commonErr.ErrInvoiceNotCompleted
+	}
+
+	transactionReview := req.ToModel()
+	err = s.transactionReviewRepo.Create(transactionReview)
 	if err != nil {
 		return err
 	}
