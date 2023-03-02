75620cd44bf42e132aeb0d51a3880b750c3c620c
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-03-02 17:42:08 +0700

    feat(shop-search): find shop by keyword service

--- internal/domain/shop/service/shop_service.go
+++ internal/domain/shop/service/shop_service.go
@@ -1,14 +1,19 @@
 package service
 
 import (
+	commonDto "kedai/backend/be-kedai/internal/common/dto"
+	"kedai/backend/be-kedai/internal/domain/shop/dto"
 	"kedai/backend/be-kedai/internal/domain/shop/model"
 	"kedai/backend/be-kedai/internal/domain/shop/repository"
+	"strings"
 )
 
 type ShopService interface {
 	FindShopById(id int) (*model.Shop, error)
 	FindShopByUserId(userId int) (*model.Shop, error)
 	FindShopBySlug(slug string) (*model.Shop, error)
+	FindShopByKeyword(req *dto.FindShopRequest) (*commonDto.PaginationResponse, error)
+	FindMostRatedShopByKeyword(keyword string) (*model.Shop, error)
 }
 
 type shopServiceImpl struct {
@@ -40,4 +45,34 @@ func (s *shopServiceImpl) FindShopBySlug(slug string) (*model.Shop, error) {
 	}
 
 	return shop, nil
+}
+
+func (s *shopServiceImpl) FindShopByKeyword(req *dto.FindShopRequest) (*commonDto.PaginationResponse, error) {
+	validateKeyword := strings.Trim(req.Keyword, " ")
+	if validateKeyword == "" {
+		return &commonDto.PaginationResponse{
+			Data:       nil,
+			TotalRows:  0,
+			TotalPages: 0,
+			Limit:      req.Limit,
+			Page:       req.Page,
+		}, nil
+	}
+
+	res, rows, pages, err := s.shopRepository.FindShopByKeyword(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &commonDto.PaginationResponse{
+		Data:       res,
+		TotalRows:  rows,
+		TotalPages: pages,
+		Limit:      req.Limit,
+		Page:       req.Page,
+	}, nil
+}
+
+func (s *shopServiceImpl) FindMostRatedShopByKeyword(keyword string) (*model.Shop, error) {
+	return s.shopRepository.FindMostRatedShopByKeyword(keyword)
 }
