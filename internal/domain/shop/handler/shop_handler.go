0adf135a69013f705a516c35179f10f482db3cb5
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-03-02 18:02:08 +0700

    feat(shop-search): find shop by keyword handler

--- internal/domain/shop/handler/shop_handler.go
+++ internal/domain/shop/handler/shop_handler.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"kedai/backend/be-kedai/internal/common/code"
 	errs "kedai/backend/be-kedai/internal/common/error"
+	"kedai/backend/be-kedai/internal/domain/shop/dto"
 	"kedai/backend/be-kedai/internal/utils/response"
 	"net/http"
 
@@ -20,6 +21,19 @@ func (h *Handler) FindShopBySlug(c *gin.Context) {
 			return
 		}
 
+		response.Error(c, http.StatusInternalServerError, code.INTERNAL_SERVER_ERROR, err.Error())
+		return
+	}
+
+	response.Success(c, http.StatusOK, code.OK, "ok", result)
+}
+
+func (h *Handler) FindShopByKeyword(c *gin.Context) {
+	var req *dto.FindShopRequest
+	_ = c.ShouldBindQuery(&req)
+
+	result, err := h.shopService.FindShopByKeyword(req)
+	if err != nil {
 		response.Error(c, http.StatusInternalServerError, code.INTERNAL_SERVER_ERROR, err.Error())
 		return
 	}
