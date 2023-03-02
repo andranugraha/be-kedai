f9ebb9f4a884417efc7fe8abb436c0aa3449e71f
Izharul Haq <izharul.haq@shopee.com>
2023-03-02 17:35:28 +0700

    fix(get-sku): fix min argument requirements to 1

--- internal/common/error/sku.go
+++ internal/common/error/sku.go
@@ -3,7 +3,6 @@ package error
 import "errors"
 
 var (
-	ErrIncompleteVariantIDArguments = errors.New("incomplete variant ID arguments")
-	ErrInvalidVariantID             = errors.New("invalid variant ID")
-	ErrSKUDoesNotExist              = errors.New("sku does not exist")
+	ErrInvalidVariantID = errors.New("invalid variant ID")
+	ErrSKUDoesNotExist  = errors.New("sku does not exist")
 )
--- internal/domain/product/dto/sku.go
+++ internal/domain/product/dto/sku.go
@@ -12,13 +12,14 @@ type GetSKURequest struct {
 
 func (d *GetSKURequest) ToIntList() ([]int, error) {
 	variantList := strings.Split(d.VariantID, ",")
-	if len(variantList) < 2 {
-		return nil, errs.ErrIncompleteVariantIDArguments
-	}
 
 	variant1, err := strconv.Atoi(variantList[0])
 	if err != nil || variant1 < 1 {
 		return nil, errs.ErrInvalidVariantID
+	}
+
+	if len(variantList) == 1 {
+		return []int{variant1}, nil
 	}
 
 	variant2, err := strconv.Atoi(variantList[1])
