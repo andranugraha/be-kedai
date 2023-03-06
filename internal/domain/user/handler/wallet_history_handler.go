32f1d1eb7141c87a82b048c726981b102626f2ee
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-03-06 20:21:59 +0700

    feat(wallet): validate pagination handler

--- internal/domain/user/handler/wallet_history_handler.go
+++ internal/domain/user/handler/wallet_history_handler.go
@@ -3,7 +3,8 @@ package handler
 import (
 	"errors"
 	"kedai/backend/be-kedai/internal/common/code"
-	errs	"kedai/backend/be-kedai/internal/common/error"
+	errs "kedai/backend/be-kedai/internal/common/error"
+	"kedai/backend/be-kedai/internal/domain/user/dto"
 	"kedai/backend/be-kedai/internal/utils/response"
 	"net/http"
 
@@ -12,8 +13,11 @@ import (
 
 func (h *Handler) GetWalletHistory(c *gin.Context) {
 	userId := c.GetInt("userId")
+	var req dto.WalletHistoryRequest
+	_ = c.ShouldBindQuery(&req)
+	req.Validate()
 
-	result, err := h.walletHistoryService.GetWalletHistoryById(userId)
+	result, err := h.walletHistoryService.GetWalletHistoryById(req, userId)
 	if err != nil {
 		if errors.Is(err, errs.ErrWalletDoesNotExist) {
 			response.Error(c, http.StatusNotFound, code.NOT_FOUND, err.Error())
