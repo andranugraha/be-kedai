e746d121c126bc685523eb5d5654a0945df5bd93
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-03-06 20:21:04 +0700

    feat(wallet): implement pagination dto

--- internal/domain/user/dto/wallet.go
+++ internal/domain/user/dto/wallet.go
@@ -8,3 +8,22 @@ type TopUpRequest struct {
 	Amount float64 `form:"amount" binding:"required,numeric,min=10000,max=20000000"`
 	TxnId  string  `form:"txn_id" binding:"required"`
 }
+
+type WalletHistoryRequest struct {
+	Page  int `form:"page"`
+	Limit int `form:"limit"`
+}
+
+func (req *WalletHistoryRequest) Validate() {
+	if req.Page < 1 {
+		req.Page = 1
+	}
+
+	if req.Limit < 1 {
+		req.Limit = 10
+	}
+}
+
+func (req *WalletHistoryRequest) Offset() int {
+	return (req.Page - 1) * req.Limit
+}
