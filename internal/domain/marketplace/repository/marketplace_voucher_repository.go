d7f73575b85fdf06d06efaba4fdda0f05c2f0113
javin.javin <javin.javin@shopee.com>
2023-03-06 10:58:14 +0700

    feat(voucher): find marketplace voucher with category or payment method

--- internal/domain/marketplace/repository/marketplace_voucher_repository.go
+++ internal/domain/marketplace/repository/marketplace_voucher_repository.go
@@ -1,35 +1,85 @@
 package repository
 
 import (
+	"kedai/backend/be-kedai/internal/domain/marketplace/dto"
 	"kedai/backend/be-kedai/internal/domain/marketplace/model"
 	"time"
+
+	userRepo "kedai/backend/be-kedai/internal/domain/user/repository"
 
 	"gorm.io/gorm"
 )
 
 type MarketplaceVoucherRepository interface {
-	GetMarketplaceVoucher() ([]*model.MarketplaceVoucher, error)
+	GetMarketplaceVoucher(req *dto.GetMarketplaceVoucherRequest) ([]*model.MarketplaceVoucher, error)
+	GetValidByUserID(req *dto.GetMarketplaceVoucherRequest) ([]*model.MarketplaceVoucher, error)
 }
 
 type marketplaceVoucherRepositoryImpl struct {
-	db *gorm.DB
+	db                    *gorm.DB
+	userVoucherRepository userRepo.UserVoucherRepository
 }
 
 type MarketplaceVoucherRConfig struct {
-	DB *gorm.DB
+	DB                    *gorm.DB
+	UserVoucherRepository userRepo.UserVoucherRepository
 }
 
 func NewMarketplaceVoucherRepository(cfg *MarketplaceVoucherRConfig) MarketplaceVoucherRepository {
 	return &marketplaceVoucherRepositoryImpl{
-		db: cfg.DB,
+		db:                    cfg.DB,
+		userVoucherRepository: cfg.UserVoucherRepository,
 	}
 }
 
-func (r *marketplaceVoucherRepositoryImpl) GetMarketplaceVoucher() ([]*model.MarketplaceVoucher, error) {
+func (r *marketplaceVoucherRepositoryImpl) GetMarketplaceVoucher(req *dto.GetMarketplaceVoucherRequest) ([]*model.MarketplaceVoucher, error) {
 	var marketplaceVoucher []*model.MarketplaceVoucher
 
+	db := r.db
+
+	if req.CategoryId != 0 {
+		db = db.Where("category_id = ?", req.CategoryId)
+	}
+	if req.PaymentMethodId != 0 {
+		db = db.Where("payment_method_id = ?", req.PaymentMethodId)
+	}
+
 	publicVoucher := true
-	err := r.db.Where("expired_at > ?", time.Now()).Where("is_hidden != ?", publicVoucher).Find(&marketplaceVoucher).Error
+	err := db.Where("expired_at > ?", time.Now()).Where("is_hidden != ?", publicVoucher).Find(&marketplaceVoucher).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return marketplaceVoucher, nil
+}
+
+func (r *marketplaceVoucherRepositoryImpl) GetValidByUserID(req *dto.GetMarketplaceVoucherRequest) ([]*model.MarketplaceVoucher, error) {
+	var marketplaceVoucher []*model.MarketplaceVoucher
+	var invalidVoucherID []int
+
+	userVoucher, err := r.userVoucherRepository.GetUsedMarketplaceByUserID(req.UserId)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, voucher := range userVoucher {
+		invalidVoucherID = append(invalidVoucherID, voucher.MarketplaceVoucherId)
+	}
+
+	db := r.db
+
+	if req.CategoryId != 0 {
+		db = db.Where("category_id = ?", req.CategoryId)
+	}
+	if req.PaymentMethodId != 0 {
+		db = db.Where("payment_method_id = ?", req.PaymentMethodId)
+	}
+
+	publicVoucher := true
+	err = db.Where("expired_at > ?", time.Now()).
+		Where("is_hidden != ?", publicVoucher).
+		Not("id IN (?)", invalidVoucherID).
+		Find(&marketplaceVoucher).Error
 	if err != nil {
 		return nil, err
 	}
