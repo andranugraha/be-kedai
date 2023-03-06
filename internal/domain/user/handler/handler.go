892c146b32f8131dca1e830504bad8fc3c362ab6
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-03-06 18:00:37 +0700

    feat(wallet): add wallet history service

--- internal/domain/user/handler/handler.go
+++ internal/domain/user/handler/handler.go
@@ -3,33 +3,36 @@ package handler
 import "kedai/backend/be-kedai/internal/domain/user/service"
 
 type Handler struct {
-	userService         service.UserService
-	userWishlistService service.UserWishlistService
-	userCartItemService service.UserCartItemService
-	walletService       service.WalletService
-	sealabsPayService   service.SealabsPayService
-	userAddressService  service.UserAddressService
-	userProfileService  service.UserProfileService
+	userService          service.UserService
+	userWishlistService  service.UserWishlistService
+	userCartItemService  service.UserCartItemService
+	walletService        service.WalletService
+	walletHistoryService service.WalletHistoryService
+	sealabsPayService    service.SealabsPayService
+	userAddressService   service.UserAddressService
+	userProfileService   service.UserProfileService
 }
 
 type HandlerConfig struct {
-	UserService         service.UserService
-	UserWishlistService service.UserWishlistService
-	UserCartItemService service.UserCartItemService
-	WalletService       service.WalletService
-	SealabsPayService   service.SealabsPayService
-	UserAddressService  service.UserAddressService
-	UserProfileService  service.UserProfileService
+	UserService          service.UserService
+	UserWishlistService  service.UserWishlistService
+	UserCartItemService  service.UserCartItemService
+	WalletService        service.WalletService
+	WalletHistoryService service.WalletHistoryService
+	SealabsPayService    service.SealabsPayService
+	UserAddressService   service.UserAddressService
+	UserProfileService   service.UserProfileService
 }
 
 func New(cfg *HandlerConfig) *Handler {
 	return &Handler{
-		userService:         cfg.UserService,
-		userWishlistService: cfg.UserWishlistService,
-		userCartItemService: cfg.UserCartItemService,
-		walletService:       cfg.WalletService,
-		sealabsPayService:   cfg.SealabsPayService,
-		userAddressService:  cfg.UserAddressService,
-		userProfileService:  cfg.UserProfileService,
+		userService:          cfg.UserService,
+		userWishlistService:  cfg.UserWishlistService,
+		userCartItemService:  cfg.UserCartItemService,
+		walletService:        cfg.WalletService,
+		walletHistoryService: cfg.WalletHistoryService,
+		sealabsPayService:    cfg.SealabsPayService,
+		userAddressService:   cfg.UserAddressService,
+		userProfileService:   cfg.UserProfileService,
 	}
 }
