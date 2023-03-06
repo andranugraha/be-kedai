36def80c20f106593bde4446ee29ca80f03f8e33
javin.javin <javin.javin@shopee.com>
2023-03-06 10:58:31 +0700

    feat(voucher): service tests

--- internal/domain/marketplace/service/marketplace_voucher_service_test.go
+++ internal/domain/marketplace/service/marketplace_voucher_service_test.go
@@ -1,6 +1,7 @@
 package service_test
 
 import (
+	"kedai/backend/be-kedai/internal/domain/marketplace/dto"
 	"kedai/backend/be-kedai/internal/domain/marketplace/model"
 	"kedai/backend/be-kedai/internal/domain/marketplace/service"
 	"kedai/backend/be-kedai/mocks"
@@ -11,6 +12,7 @@ import (
 
 func TestGetMarketplaceVoucher(t *testing.T) {
 	type input struct {
+		req        *dto.GetMarketplaceVoucherRequest
 		err        error
 		beforeTest func(*mocks.MarketplaceVoucherRepository)
 	}
@@ -28,8 +30,9 @@ func TestGetMarketplaceVoucher(t *testing.T) {
 			description: "should return error and marketplace vouchers",
 			input: input{
 				err: nil,
+				req: &dto.GetMarketplaceVoucherRequest{},
 				beforeTest: func(m *mocks.MarketplaceVoucherRepository) {
-					m.On("GetMarketplaceVoucher").Return(nil, nil)
+					m.On("GetMarketplaceVoucher", &dto.GetMarketplaceVoucherRequest{}).Return(nil, nil)
 				},
 			},
 			expected: expected{
@@ -48,7 +51,58 @@ func TestGetMarketplaceVoucher(t *testing.T) {
 				MarketplaceVoucherRepository: m,
 			})
 
-			result, err := s.GetMarketplaceVoucher()
+			result, err := s.GetMarketplaceVoucher(c.input.req)
+
+			assert.Equal(t, c.expected.err, err)
+			assert.Equal(t, c.expected.result, result)
+
+		})
+	}
+
+}
+
+func TestGetValidByUserID(t *testing.T) {
+	type input struct {
+		req        *dto.GetMarketplaceVoucherRequest
+		err        error
+		beforeTest func(*mocks.MarketplaceVoucherRepository)
+	}
+	type expected struct {
+		result []*model.MarketplaceVoucher
+		err    error
+	}
+
+	cases := []struct {
+		description string
+		input
+		expected
+	}{
+		{
+			description: "should return error and marketplace vouchers",
+			input: input{
+				err: nil,
+				req: &dto.GetMarketplaceVoucherRequest{},
+				beforeTest: func(m *mocks.MarketplaceVoucherRepository) {
+					m.On("GetValidByUserID", &dto.GetMarketplaceVoucherRequest{}).Return(nil, nil)
+				},
+			},
+			expected: expected{
+				result: nil,
+				err:    nil,
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.description, func(t *testing.T) {
+			m := mocks.NewMarketplaceVoucherRepository(t)
+			c.beforeTest(m)
+
+			s := service.NewMarketplaceVoucherService(&service.MarketplaceVoucherSConfig{
+				MarketplaceVoucherRepository: m,
+			})
+
+			result, err := s.GetValidByUserID(c.input.req)
 
 			assert.Equal(t, c.expected.err, err)
 			assert.Equal(t, c.expected.result, result)
