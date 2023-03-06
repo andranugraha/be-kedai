d5cb6c0ac4150e6e66fd335a13c1b5d1529d5148
Izharul Haq <izharul.haq@shopee.com>
2023-03-06 15:53:52 +0700

    feat(invoice-filter): add payment date field

--- internal/domain/order/model/invoice.go
+++ internal/domain/order/model/invoice.go
@@ -1,19 +1,22 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Invoice struct {
-	ID              int      `json:"id"`
-	Code            string   `json:"code"`
-	Subtotal        float64  `json:"subtotal"`
-	Total           float64  `json:"total"`
-	VoucherAmount   *float64 `json:"voucherAmount,omitempty"`
-	VoucherType     *string  `json:"voucherType,omitempty"`
-	UserID          int      `json:"userId"`
-	VoucherID       *int     `json:"voucherId,omitempty"`
-	PaymentMethodID int      `json:"courierServiceId"`
-
-	InvoicePerShop []*InvoicePerShop `json:"invoicePerShop"`
+	ID              int       `json:"id"`
+	Code            string    `json:"code"`
+	Subtotal        float64   `json:"subtotal"`
+	Total           float64   `json:"total"`
+	VoucherAmount   *float64  `json:"voucherAmount,omitempty"`
+	VoucherType     *string   `json:"voucherType,omitempty"`
+	UserID          int       `json:"userId"`
+	VoucherID       *int      `json:"voucherId,omitempty"`
+	PaymentMethodID int       `json:"courierServiceId"`
+	PaymentDate     time.Time `json:"paymentDate"`
 
 	gorm.Model `json:"-"`
 }
