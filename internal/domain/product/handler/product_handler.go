cee8829717771632331d573fe7d1fe8f19527d1f
Izharul Haq <izharul.haq@shopee.com>
2023-03-06 11:22:24 +0700

    feat(get-shop-product): implement get shop product list handler

--- internal/domain/product/handler/product_handler.go
+++ internal/domain/product/handler/product_handler.go
@@ -63,3 +63,28 @@ func (h *Handler) ProductSearchFiltering(c *gin.Context) {
 
 	response.Success(c, http.StatusOK, code.OK, "ok", product)
 }
+
+func (h *Handler) GetProductsByShopSlug(c *gin.Context) {
+	var request dto.ShopProductFilterRequest
+	err := c.ShouldBindQuery(&request)
+	if err != nil {
+		response.ErrorValidator(c, http.StatusBadRequest, err)
+	}
+
+	request.Validate()
+
+	slug := c.Param("slug")
+
+	res, err := h.productService.GetProductsByShopSlug(slug, &request)
+	if err != nil {
+		if errors.Is(err, errs.ErrShopNotFound) {
+			response.Error(c, http.StatusNotFound, code.NOT_FOUND, err.Error())
+			return
+		}
+
+		response.Error(c, http.StatusInternalServerError, code.INTERNAL_SERVER_ERROR, err.Error())
+		return
+	}
+
+	response.Success(c, http.StatusOK, code.OK, "success", res)
+}
