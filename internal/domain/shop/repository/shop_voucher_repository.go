f4d1ab94f64f091e1246b210ce9ce2b746b99a70
javin.javin <javin.javin@shopee.com>
2023-03-06 10:59:34 +0700

    feat(voucher): repo for get valid shop voucher

--- internal/domain/shop/repository/shop_voucher_repository.go
+++ internal/domain/shop/repository/shop_voucher_repository.go
@@ -4,24 +4,30 @@ import (
 	"kedai/backend/be-kedai/internal/domain/shop/model"
 	"time"
 
+	userRepo "kedai/backend/be-kedai/internal/domain/user/repository"
+
 	"gorm.io/gorm"
 )
 
 type ShopVoucherRepository interface {
 	GetShopVoucher(shopId int) ([]*model.ShopVoucher, error)
+	GetValidByUserIDAndShopID(userID int, shopID int) ([]*model.ShopVoucher, error)
 }
 
 type shopVoucherRepositoryImpl struct {
-	db *gorm.DB
+	db                    *gorm.DB
+	userVoucherRepository userRepo.UserVoucherRepository
 }
 
 type ShopVoucherRConfig struct {
-	DB *gorm.DB
+	DB                    *gorm.DB
+	UserVoucherRepository userRepo.UserVoucherRepository
 }
 
 func NewShopVoucherRepository(cfg *ShopVoucherRConfig) ShopVoucherRepository {
 	return &shopVoucherRepositoryImpl{
-		db: cfg.DB,
+		db:                    cfg.DB,
+		userVoucherRepository: cfg.UserVoucherRepository,
 	}
 }
 
@@ -35,4 +41,30 @@ func (r *shopVoucherRepositoryImpl) GetShopVoucher(shopId int) ([]*model.ShopVou
 	}
 
 	return shopVoucher, nil
+}
+
+func (r *shopVoucherRepositoryImpl) GetValidByUserIDAndShopID(userID int, shopID int) ([]*model.ShopVoucher, error) {
+	var shopVouchers []*model.ShopVoucher
+	var invalidVoucherID []int
+
+	userVoucher, err := r.userVoucherRepository.GetUsedShopByUserID(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, voucher := range userVoucher {
+		invalidVoucherID = append(invalidVoucherID, voucher.MarketplaceVoucherId)
+	}
+
+	publicVoucher := true
+	err = r.db.Where("shop_id = ?", shopID).
+		Where("is_hidden != ?", publicVoucher).
+		Where("? < expired_at", time.Now()).
+		Not("id IN (?)", invalidVoucherID).
+		Find(&shopVouchers).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return shopVouchers, nil
 }
