9030ba5f5338a8a721859773da9a93eaadf33d81
Izharul Haq <izharul.haq@shopee.com>
2023-03-07 11:47:08 +0700

    refactor(invoice-filter): rename voucher fields in invoice per shop

--- internal/domain/order/model/invoice_per_shop.go
+++ internal/domain/order/model/invoice_per_shop.go
@@ -7,23 +7,23 @@ import (
 )
 
 type InvoicePerShop struct {
-	ID               int      `json:"id"`
-	Code             string   `json:"code"`
-	Subtotal         float64  `json:"subtotal"`
-	Total            float64  `json:"total"`
-	ShippingCost     float64  `json:"shippingCost"`
-	TrackingNumber   int      `json:"trackingNumber"`
-	PromotionAmount  *float64 `json:"promotionAmount,omitempty"`
-	PromotionType    *string  `json:"promotionType,omitempty"`
-	VoucherAmount    *float64 `json:"voucherAmount,omitempty"`
-	VoucherType      *string  `json:"voucherType,omitempty"`
-	Status           string   `json:"status"`
-	PromotionID      *int     `json:"promotionId,omitempty"`
-	ShopID           int      `json:"shopId"`
-	UserID           int      `json:"userId"`
-	VoucherID        *int     `json:"voucherId,omitempty"`
-	InvoiceID        int      `json:"invoiceId"`
-	CourierServiceID int      `json:"courierServiceId"`
+	ID                int      `json:"id"`
+	Code              string   `json:"code"`
+	Subtotal          float64  `json:"subtotal"`
+	Total             float64  `json:"total"`
+	ShippingCost      float64  `json:"shippingCost"`
+	TrackingNumber    int      `json:"trackingNumber"`
+	PromotionAmount   *float64 `json:"promotionAmount,omitempty"`
+	PromotionType     *string  `json:"promotionType,omitempty"`
+	ShopVoucherAmount *float64 `json:"shopVoucherAmount,omitempty" gorm:"column:voucher_amount"`
+	ShopVoucherType   *string  `json:"shopVoucherType,omitempty" gorm:"column:voucher_type"`
+	Status            string   `json:"status"`
+	PromotionID       *int     `json:"promotionId,omitempty"`
+	ShopID            int      `json:"shopId"`
+	UserID            int      `json:"userId"`
+	VoucherID         *int     `json:"voucherId,omitempty"`
+	InvoiceID         int      `json:"invoiceId"`
+	CourierServiceID  int      `json:"courierServiceId"`
 
 	Shop *model.Shop `json:"shop,omitempty"`
 
