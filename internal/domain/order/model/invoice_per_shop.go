d735547ec685e0c55c8bac7f3e300a3cff81f571
Izharul Haq <izharul.haq@shopee.com>
2023-03-07 16:01:05 +0700

    refactor(invoice-filter): remove unnecessary fields

--- internal/domain/order/model/invoice_per_shop.go
+++ internal/domain/order/model/invoice_per_shop.go
@@ -7,22 +7,19 @@ import (
 )
 
 type InvoicePerShop struct {
-	ID              int     `json:"id"`
-	Code            string  `json:"code"`
-	Total           float64 `json:"total"`
-	Subtotal        float64 `json:"subtotal"`
-	ShippingCost    float64 `json:"shippingCost"`
-	VoucherAmount   float64 `json:"voucherAmount"`
-	VoucherType     string  `json:"voucherType"`
-	PromotionAmount float64 `json:"promotionAmount"`
-	PromotionType   string  `json:"promotionType"`
-	Status          string  `json:"status"`
+	ID            int     `json:"id"`
+	Code          string  `json:"code"`
+	Total         float64 `json:"total"`
+	Subtotal      float64 `json:"subtotal"`
+	ShippingCost  float64 `json:"shippingCost"`
+	VoucherAmount float64 `json:"voucherAmount"`
+	VoucherType   string  `json:"voucherType"`
+	Status        string  `json:"status"`
 
-	UserID      int `json:"userId"`
-	VoucherID   int `json:"voucherId"`
-	ShopID      int `json:"shopId"`
-	PromotionID int `json:"promotionId"`
-	InvoiceID   int `json:"invoiceId"`
+	UserID    int `json:"userId"`
+	VoucherID int `json:"voucherId"`
+	ShopID    int `json:"shopId"`
+	InvoiceID int `json:"invoiceId"`
 
 	Shop *model.Shop `json:"shop,omitempty"`
 
