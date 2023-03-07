e85368fcfed0d158aefe6e4f93123167f53e61c9
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-03-07 15:13:15 +0700

    feat(shipment): get all couriers handler

---
+++ internal/domain/shop/handler/courier_handler.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"kedai/backend/be-kedai/internal/common/code"
+	"kedai/backend/be-kedai/internal/utils/response"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) GetAllCourier(c *gin.Context) {
+	result, err := h.courierService.GetAllCouriers()
+	if err != nil {
+		response.Error(c, http.StatusInternalServerError, code.INTERNAL_SERVER_ERROR, err.Error())
+		return
+	}
+
+	response.Success(c, http.StatusOK, code.OK, "ok", result)
+}
