5756ef1268e633cbe29cf1ee53e80783ff1bd675
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-03-08 17:59:54 +0700

    feat(shipment): add shopId as query param

--- internal/domain/shop/handler/courier_handler.go
+++ internal/domain/shop/handler/courier_handler.go
@@ -4,12 +4,15 @@ import (
 	"kedai/backend/be-kedai/internal/common/code"
 	"kedai/backend/be-kedai/internal/utils/response"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) GetAllCouriers(c *gin.Context) {
-	result, err := h.courierService.GetAllCouriers()
+	shopId, _ := strconv.Atoi(c.Query("shopId"))
+
+	result, err := h.courierService.GetShipmentList(shopId)
 	if err != nil {
 		response.Error(c, http.StatusInternalServerError, code.INTERNAL_SERVER_ERROR, err.Error())
 		return
