7bba1e9fbdf6f4aa663e76b8d3416c4b4b899cbe
Izharul Haq <izharul.haq@shopee.com>
2023-03-08 13:41:09 +0700

    feat(change-wallet-pin): implement invalid pin error

--- internal/common/error/wallet.go
+++ internal/common/error/wallet.go
@@ -4,6 +4,7 @@ import "errors"
 
 var (
 	ErrInvalidPin                = errors.New("pin must be numeric and have 6 characters")
+	ErrPinMismatch               = errors.New("pin is not valid")
 	ErrWalletAlreadyExist        = errors.New("user only allowed to have one wallet")
 	ErrWalletDoesNotExist        = errors.New("user does not have any wallet yet")
 	ErrWalletHistoryDoesNotExist = errors.New("wallet transaction history not exist")
