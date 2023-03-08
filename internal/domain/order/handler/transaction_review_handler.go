7bf9c1e0559139c14ad18aa1311fe1a8099522e7
Izharul Haq <izharul.haq@shopee.com>
2023-03-08 10:06:21 +0700

    feat(transaction-review): implement get transaction review handler

--- internal/domain/order/handler/transaction_review_handler.go
+++ internal/domain/order/handler/transaction_review_handler.go
@@ -6,6 +6,7 @@ import (
 	"kedai/backend/be-kedai/internal/domain/order/dto"
 	"kedai/backend/be-kedai/internal/utils/response"
 	"net/http"
+	"strconv"
 
 	commonErr "kedai/backend/be-kedai/internal/common/error"
 
@@ -42,4 +43,26 @@ func (h *Handler) AddTransactionReview(c *gin.Context) {
 	}
 
 	response.Success(c, http.StatusCreated, code.CREATED, "created", review)
+}
+
+func (h *Handler) GetReviewByTransactionID(c *gin.Context) {
+	id := c.Param("transactionId")
+	transactionID, err := strconv.Atoi(id)
+	if err != nil || transactionID < 1 {
+		response.Error(c, http.StatusBadRequest, code.BAD_REQUEST, commonErr.ErrInvalidTransactionID.Error())
+		return
+	}
+
+	review, err := h.transactionReviewService.GetReviewByTransactionID(transactionID)
+	if err != nil {
+		if errors.Is(err, commonErr.ErrTransactionReviewNotFound) {
+			response.Error(c, http.StatusNotFound, code.NOT_FOUND, err.Error())
+			return
+		}
+
+		response.Error(c, http.StatusInternalServerError, code.INTERNAL_SERVER_ERROR, commonErr.ErrInternalServerError.Error())
+		return
+	}
+
+	response.Success(c, http.StatusOK, code.OK, "success", review)
 }
