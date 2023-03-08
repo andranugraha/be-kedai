eed2ed4197cea6dab4921410c89c0f76edd85cfb
Izharul Haq <izharul.haq@shopee.com>
2023-03-08 15:21:47 +0700

    fix(invoice-detail): add missing tracking number field

--- internal/domain/order/model/invoice_per_shop.go
+++ internal/domain/order/model/invoice_per_shop.go
@@ -8,14 +8,15 @@ import (
 )
 
 type InvoicePerShop struct {
-	ID            int     `json:"id"`
-	Code          string  `json:"code"`
-	Total         float64 `json:"total"`
-	Subtotal      float64 `json:"subtotal"`
-	ShippingCost  float64 `json:"shippingCost"`
-	VoucherAmount float64 `json:"voucherAmount"`
-	VoucherType   string  `json:"voucherType"`
-	Status        string  `json:"status"`
+	ID             int     `json:"id"`
+	Code           string  `json:"code"`
+	Total          float64 `json:"total"`
+	Subtotal       float64 `json:"subtotal"`
+	ShippingCost   float64 `json:"shippingCost"`
+	TrackingNumber int     `json:"trackingNumber"`
+	VoucherAmount  float64 `json:"voucherAmount"`
+	VoucherType    string  `json:"voucherType"`
+	Status         string  `json:"status"`
 
 	UserID           int `json:"userId"`
 	VoucherID        int `json:"voucherId"`
