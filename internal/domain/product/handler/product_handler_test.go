64fea24abee551ceaed12628f220d8ac6dd23c40
Izharul Haq <izharul.haq@shopee.com>
2023-03-09 15:15:31 +0700

    test(seller-product): implement get seller products handler unit test

--- internal/domain/product/handler/product_handler_test.go
+++ internal/domain/product/handler/product_handler_test.go
@@ -415,3 +415,115 @@ func TestGetProductsByShopSlug(t *testing.T) {
 		})
 	}
 }
+
+func TestGetSellerProduct(t *testing.T) {
+	type input struct {
+		userID   int
+		request  *dto.SellerProductFilterRequest
+		mockData *commonDto.PaginationResponse
+		mockErr  error
+	}
+	type expected struct {
+		statusCode int
+		response   response.Response
+	}
+
+	var (
+		userID     = 1
+		page       = 2
+		limit      = 10
+		totalRows  = int64(0)
+		products   = []*dto.SellerProduct{}
+		totalPages = 0
+		request    = &dto.SellerProductFilterRequest{
+			Page:  page,
+			Limit: limit,
+		}
+	)
+
+	tests := []struct {
+		description string
+		input
+		expected
+	}{
+		{
+			description: "should return error with status code 404 when shop does not exist",
+			input: input{
+				userID:   userID,
+				request:  request,
+				mockData: nil,
+				mockErr:  errs.ErrShopNotFound,
+			},
+			expected: expected{
+				statusCode: http.StatusNotFound,
+				response: response.Response{
+					Code:    code.SHOP_NOT_REGISTERED,
+					Message: errs.ErrShopNotFound.Error(),
+				},
+			},
+		},
+		{
+			description: "should return error with status code 500 when something went wrong",
+			input: input{
+				userID:   userID,
+				request:  request,
+				mockData: nil,
+				mockErr:  errors.New("something went wrong"),
+			},
+			expected: expected{
+				statusCode: http.StatusInternalServerError,
+				response: response.Response{
+					Code:    code.INTERNAL_SERVER_ERROR,
+					Message: errs.ErrInternalServerError.Error(),
+				},
+			},
+		},
+		{
+			description: "should return error with status code 200 when suceed fetching products",
+			input: input{
+				userID:  userID,
+				request: request,
+				mockData: &commonDto.PaginationResponse{
+					TotalRows:  totalRows,
+					TotalPages: totalPages,
+					Page:       page,
+					Limit:      limit,
+					Data:       products,
+				},
+				mockErr: nil,
+			},
+			expected: expected{
+				statusCode: http.StatusOK,
+				response: response.Response{
+					Code:    code.OK,
+					Message: "success",
+					Data: &commonDto.PaginationResponse{
+						TotalRows:  totalRows,
+						TotalPages: totalPages,
+						Page:       page,
+						Limit:      limit,
+						Data:       products,
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		expectedRes, _ := json.Marshal(tc.expected.response)
+		productService := mocks.NewProductService(t)
+		productService.On("GetSellerProducts", tc.input.userID, tc.input.request).Return(tc.input.mockData, tc.input.mockErr)
+		rec := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(rec)
+		c.Set("userId", tc.input.userID)
+		h := handler.New(&handler.Config{
+			ProductService: productService,
+		})
+		c.Request = httptest.NewRequest("GET", fmt.Sprintf("/v1/sellers/products?page=%d&limit=%d", tc.input.request.Page, tc.input.request.Limit), nil)
+
+		h.GetSellerProducts(c)
+
+		assert.Equal(t, tc.expected.statusCode, rec.Code)
+		assert.Equal(t, string(expectedRes), rec.Body.String())
+	}
+}
