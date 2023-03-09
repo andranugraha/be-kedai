152aba3fa8ea4fbcfdfa3fb2c3294b420c15dd95
javin.javin <javin.javin@shopee.com>
2023-03-09 10:31:43 +0700

    fix(review): add dependency

--- internal/server/server.go
+++ internal/server/server.go
@@ -232,6 +232,7 @@ func createRouter() *gin.Engine {
 		TransactionReviewRepo: transactionReviewRepo,
 		TransactionService:    transactionService,
 		InvoicePerShopService: invoicePerShopService,
+		ProductService:        productService,
 	})
 
 	sealabsPayRepo := userRepoPackage.NewSealabsPayRepository(&userRepoPackage.SealabsPayRConfig{
