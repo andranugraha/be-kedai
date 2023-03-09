ea3f8e9150fe7effbfceb8cc1bbc1fe5131ce629
Izharul Haq <izharul.haq@shopee.com>
2023-03-09 15:14:28 +0700

    feat(seller-product): implement seller product sort and status query param values

--- internal/common/constant/product_sort.go
+++ internal/common/constant/product_sort.go
@@ -4,6 +4,9 @@ const (
 	SortByRecommended = "recommended"
 	SortByLatest      = "latest"
 	SortByTopSales    = "top_sales"
+	SortByLowSales    = "low_sales"
+	SortByStockHigh   = "stock_high"
+	SortByStockLow    = "stock_low"
 	SortByPriceLow    = "price_low"
 	SortByPriceHigh   = "price_high"
 )
