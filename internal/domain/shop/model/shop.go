5907c6d5eaa2f9c404e9b6c4664b185d1bf9f038
Izharul Haq <izharul.haq@shopee.com>
2023-03-14 08:17:04 +0700

    feat(shop-registration): add shop couriers field

--- internal/domain/shop/model/shop.go
+++ internal/domain/shop/model/shop.go
@@ -9,17 +9,18 @@ import (
 )
 
 type Shop struct {
-	ID           int                        `json:"id"`
-	Name         string                     `json:"name"`
-	Rating       float64                    `json:"rating"`
-	Description  *string                    `json:"description"`
-	PhotoUrl     *string                    `json:"photoUrl"`
-	JoinedDate   time.Time                  `json:"joinedDate"`
-	UserID       int                        `json:"userId"`
-	AddressID    int                        `json:"addressId"`
-	Address      *locationModel.UserAddress `json:"address,omitempty"`
-	Slug         string                     `json:"slug"`
-	ShopCategory []*ShopCategory            `json:"shopCategories,omitempty"`
+	ID             int                        `json:"id"`
+	Name           string                     `json:"name"`
+	Rating         float64                    `json:"rating"`
+	Description    *string                    `json:"description"`
+	PhotoUrl       *string                    `json:"photoUrl"`
+	JoinedDate     time.Time                  `json:"joinedDate"`
+	UserID         int                        `json:"userId"`
+	AddressID      int                        `json:"addressId"`
+	Address        *locationModel.UserAddress `json:"address,omitempty"`
+	Slug           string                     `json:"slug"`
+	ShopCategory   []*ShopCategory            `json:"shopCategories,omitempty"`
+	CourierService []*CourierService          `gorm:"many2many:shop_couriers"`
 
 	gorm.Model `json:"-"`
 }
