dc65d2cbcbdb163093565862f7d8e79df1bbcf91
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-03-14 17:39:19 +0700

    test(order): update to receive handler unit test

--- internal/domain/order/handler/invoice_per_shop_handler_test.go
+++ internal/domain/order/handler/invoice_per_shop_handler_test.go
@@ -936,10 +936,99 @@ func TestUpdateToCancelled(t *testing.T) {
 	}
 }
 
+func TestUpdateToReceived(t *testing.T) {
+	var (
+		userId    = 1
+		orderCode = "code"
+	)
+	type input struct {
+		userId    int
+		orderCode string
+		err       error
+	}
+	type expected struct {
+		statusCode int
+		response   response.Response
+	}
+	type cases struct {
+		description string
+		input
+		expected
+	}
+
+	for _, tc := range []cases{
+		{
+			description: "should return nil error with code 200 when success",
+			input: input{
+				userId:    userId,
+				orderCode: orderCode,
+				err:       nil,
+			},
+			expected: expected{
+				statusCode: http.StatusOK,
+				response: response.Response{
+					Code:    code.OK,
+					Message: "ok",
+				},
+			},
+		},
+		{
+			description: "should return error with code 404 when invoice not found",
+			input: input{
+				userId:    1,
+				orderCode: orderCode,
+				err:       errs.ErrInvoiceNotFound,
+			},
+			expected: expected{
+				statusCode: http.StatusNotFound,
+				response: response.Response{
+					Code:    code.INVOICE_NOT_FOUND,
+					Message: errs.ErrInvoiceNotFound.Error(),
+				},
+			},
+		},
+		{
+			description: "should return error with code 500 when internal server error",
+			input: input{
+				userId:    1,
+				orderCode: orderCode,
+				err:       errs.ErrInternalServerError,
+			},
+			expected: expected{
+				statusCode: http.StatusInternalServerError,
+				response: response.Response{
+					Code:    code.INTERNAL_SERVER_ERROR,
+					Message: errs.ErrInternalServerError.Error(),
+				},
+			},
+		},
+	} {
+		t.Run(tc.description, func(t *testing.T) {
+			expectedJson, _ := json.Marshal(tc.expected.response)
+			invoicePerShopService := mocks.NewInvoicePerShopService(t)
+			invoicePerShopService.On("UpdateStatusToReceived", tc.input.userId, tc.input.orderCode).Return(tc.input.err)
+			rec := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(rec)
+			c.Set("userId", userId)
+			c.AddParam("code", orderCode)
+			c.Request, _ = http.NewRequest(http.MethodGet, fmt.Sprintf("/orders/invoices/{%s}/receive", tc.orderCode), nil)
+			handler := handler.New(&handler.Config{
+				InvoicePerShopService: invoicePerShopService,
+			})
+
+			handler.UpdateToReceived(c)
+
+			assert.Equal(t, tc.expected.statusCode, rec.Code)
+			assert.Equal(t, string(expectedJson), rec.Body.String())
+		})
+	}
+}
+
 func TestUpdateCronJob(t *testing.T) {
 	t.Run("should return nothing when called whether its error or success", func(t *testing.T) {
 		mockService := new(mocks.InvoicePerShopService)
 		mockService.On("UpdateStatusCRONJob").Return(nil)
+		mockService.On("AutoReceivedCRONJob").Return(nil)
 		handler := handler.New(&handler.Config{
 			InvoicePerShopService: mockService,
 		})
@@ -949,5 +1038,6 @@ func TestUpdateCronJob(t *testing.T) {
 		handler.UpdateCronJob(c)
 
 		mockService.AssertNumberOfCalls(t, "UpdateStatusCRONJob", 1)
+		mockService.AssertNumberOfCalls(t, "AutoReceivedCRONJob", 1)
 	})
 }
