f56c1adbe0d440c07febc117d1482a7d22aafbfc
Izharul Haq <izharul.haq@shopee.com>
2023-03-14 08:18:07 +0700

    feat(shop-registration): implement get courier service by IDs repository

---
+++ internal/domain/shop/repository/courier_service_repository.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"kedai/backend/be-kedai/internal/domain/shop/model"
+
+	"gorm.io/gorm"
+)
+
+type CourierServiceRepository interface {
+	GetByCourierIDs(courierIDs []int) ([]*model.CourierService, error)
+}
+
+type courierServiceRepositoryImpl struct {
+	db *gorm.DB
+}
+
+type CourierServiceRConfig struct {
+	DB *gorm.DB
+}
+
+func NewCourierServiceRepository(cfg *CourierServiceRConfig) CourierServiceRepository {
+	return &courierServiceRepositoryImpl{
+		db: cfg.DB,
+	}
+}
+
+func (r *courierServiceRepositoryImpl) GetByCourierIDs(courierIDs []int) ([]*model.CourierService, error) {
+	var courierServices []*model.CourierService
+
+	err := r.db.Where("courier_id IN ?", courierIDs).Find(&courierServices).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return courierServices, nil
+}
