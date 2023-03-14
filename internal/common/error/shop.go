d64538c8b159e245fbc15e9ea0ef523278750414
Izharul Haq <izharul.haq@shopee.com>
2023-03-14 12:43:39 +0700

    feat(shop-registration): implement create shop repository

--- internal/common/error/shop.go
+++ internal/common/error/shop.go
@@ -5,4 +5,6 @@ import "errors"
 var (
 	ErrShopNotFound    = errors.New("shop not found")
 	ErrUserIsShopOwner = errors.New("user is shop owner")
+	ErrInvalidShopName = errors.New("invalid shop name pattern")
+	ErrShopRegistered  = errors.New("shop already registered")
 )
