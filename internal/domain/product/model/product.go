8024d9f4473219834251eb59bca4b6f3d7d6b1c9
Izharul Haq <izharul.haq@shopee.com>
2023-03-15 10:01:13 +0700

    refactor(add-product): remove unused packagedSize field

--- internal/domain/product/model/product.go
+++ internal/domain/product/model/product.go
@@ -7,21 +7,20 @@ import (
 )
 
 type Product struct {
-	ID           int     `json:"id"`
-	Code         string  `json:"code"`
-	Name         string  `json:"name"`
-	Description  string  `json:"description"`
-	View         int     `json:"view"`
-	IsHazardous  bool    `json:"isHazardous"`
-	Weight       float64 `json:"weight"`
-	Length       float64 `json:"length"`
-	Width        float64 `json:"width"`
-	Height       float64 `json:"height"`
-	PackagedSize float64 `json:"packagedSize"`
-	IsNew        bool    `json:"isNew"`
-	IsActive     bool    `json:"isActive"`
-	Rating       float64 `json:"rating"`
-	Sold         int     `json:"sold"`
+	ID          int     `json:"id"`
+	Code        string  `json:"code"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	View        int     `json:"view"`
+	IsHazardous bool    `json:"isHazardous"`
+	Weight      float64 `json:"weight"`
+	Length      float64 `json:"length"`
+	Width       float64 `json:"width"`
+	Height      float64 `json:"height"`
+	IsNew       bool    `json:"isNew"`
+	IsActive    bool    `json:"isActive"`
+	Rating      float64 `json:"rating"`
+	Sold        int     `json:"sold"`
 
 	ShopID       int               `json:"shopId"`
 	Shop         *shopModel.Shop   `json:"shop,omitempty"`
