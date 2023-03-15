4b2dc7fc9892da24c8fff19775975afd9d4c1933
Izharul Haq <izharul.haq@shopee.com>
2023-03-15 12:34:25 +0700

    feat(add-product): implement function to convert request to sku

--- internal/domain/product/dto/sku.go
+++ internal/domain/product/dto/sku.go
@@ -2,6 +2,8 @@ package dto
 
 import (
 	errs "kedai/backend/be-kedai/internal/common/error"
+	"kedai/backend/be-kedai/internal/domain/product/model"
+	"kedai/backend/be-kedai/internal/utils/random"
 	"strconv"
 	"strings"
 )
@@ -32,7 +34,45 @@ func (d *GetSKURequest) ToIntList() ([]int, error) {
 
 type CreateSKURequest struct {
 	Sku        string  `json:"sku" binding:"omitempty,max=16"`
-	VariantIDs []int   `json:"variantIds" binding:"required,max=2,dive,gte=1"`
+	VariantIDs []int   `json:"variantIds" binding:"required,max=2,dive,gte=1" gorm:"-"`
 	Price      float64 `json:"price" binding:"required,gt=0"`
 	Stock      int     `json:"stock" binding:"required,gte=0"`
+	Variants   []*model.Variant
+}
+
+func (d *CreateProductRequest) GenerateSKU(variantGroup []*model.VariantGroup) []*model.Sku {
+	randomUtils := random.NewRandomUtils(&random.RandomUtilsConfig{})
+	skuLength := 16
+
+	if d.VariantGroups == nil {
+		return []*model.Sku{{
+			Price: d.Price,
+			Stock: d.Stock,
+			Sku:   randomUtils.GenerateAlphanumericString(skuLength),
+		}}
+	}
+
+	skuList := []*model.Sku{}
+	for _, s := range d.SKU {
+		var sku string
+		if s.Sku == "" {
+			sku = randomUtils.GenerateAlphanumericString(skuLength)
+		} else {
+			sku = s.Sku
+		}
+
+		variants := []model.Variant{}
+		for groupIdx, variantIdx := range s.VariantIDs {
+			variants = append(variants, *variantGroup[groupIdx].Variant[variantIdx])
+		}
+
+		skuList = append(skuList, &model.Sku{
+			Price:    s.Price,
+			Stock:    s.Stock,
+			Sku:      sku,
+			Variants: variants,
+		})
+	}
+
+	return skuList
 }
