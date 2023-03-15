ef329fef595a51d7273b470c8c670f0e677ec3da
Izharul Haq <izharul.haq@shopee.com>
2023-03-15 12:34:05 +0700

    feat(add-product): implement function to convert request to variant group

--- internal/domain/product/dto/variant_group.go
+++ internal/domain/product/dto/variant_group.go
@@ -1,7 +1,34 @@
 package dto
 
+import "kedai/backend/be-kedai/internal/domain/product/model"
+
 type CreateVariantGroupRequest struct {
 	Name     string   `json:"name" binding:"required,max=20"`
 	Options  []string `json:"options" binding:"required,min=1,max=50,dive,max=14"`
 	MediaUrl string   `json:"mediaUrl" binding:"omitempty,url"`
+}
+
+func (d *CreateProductRequest) GenerateVariantGroups() []*model.VariantGroup {
+	if d.VariantGroups == nil {
+		return nil
+	}
+
+	variantGroups := []*model.VariantGroup{}
+	for _, req := range d.VariantGroups {
+		variants := []*model.Variant{}
+		for _, v := range req.Options {
+			variants = append(variants, &model.Variant{
+				Value: v,
+			})
+		}
+
+		variants[0].MediaUrl = req.MediaUrl
+
+		variantGroups = append(variantGroups, &model.VariantGroup{
+			Name:    req.Name,
+			Variant: variants,
+		})
+	}
+
+	return variantGroups
 }
