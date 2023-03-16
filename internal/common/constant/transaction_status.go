8666c2eae26bc43b12d7c77f113e93c1880929e8
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-03-16 14:23:56 +0700

    chore: add refund constant

--- internal/common/constant/transaction_status.go
+++ internal/common/constant/transaction_status.go
@@ -1,15 +1,15 @@
 package constant
 
 const (
-	TransactionStatusWaitingForPayment  = "WAITING_FOR_PAYMENT"
-	TransactionStatusCreated            = "CREATED"
-	TransactionStatusOnDelivery         = "ON_DELIVERY"
-	TransactionStatusDelivered          = "DELIVERED"
-	TransactionStatusReceived           = "RECEIVED"
-	TransactionStatusComplained         = "COMPLAINED"
-	TransactionStatusComplaintConfirmed = "COMPLAINT_CONFIRMED"
-	TransactionStatusComplaintRejected  = "COMPLAINT_REJECTED"
-	TransactionStatusCompleted          = "COMPLETED"
-	TransactionStatusRefunded           = "REFUNDED"
-	TransactionStatusCanceled           = "CANCELED"
+	TransactionStatusWaitingForPayment = "WAITING_FOR_PAYMENT"
+	TransactionStatusCreated           = "CREATED"
+	TransactionStatusOnDelivery        = "ON_DELIVERY"
+	TransactionStatusDelivered         = "DELIVERED"
+	TransactionStatusReceived          = "RECEIVED"
+	TransactionStatusComplained        = "COMPLAINED"
+	TransactionStatusRefundPending     = "REFUND_PENDING"
+	TransactionStatusComplaintRejected = "COMPLAINT_REJECTED"
+	TransactionStatusCompleted         = "COMPLETED"
+	TransactionStatusRefunded          = "REFUNDED"
+	TransactionStatusCanceled          = "CANCELED"
 )
