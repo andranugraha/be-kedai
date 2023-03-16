0c21152163021f040cc7c0e52e9901e177782df2
javin.javin <javin.javin@shopee.com>
2023-03-16 14:12:02 +0700

    chore: add refund constants

---
+++ internal/common/constant/refund_request.go
@@ -0,0 +1,11 @@
+package constant
+
+const (
+	RefundTypeCancel   = "CANCEL"
+	RefundTypeComplain = "COMPLAIN"
+
+	RefundStatusRejected        = "REJECTED"
+	RefundStatusPending         = "PENDING"
+	RequestStatusSellerApproved = "SELLER_APPROVED"
+	RefundStatusRefunded        = "REFUNDED"
+)
