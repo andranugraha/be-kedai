1004a30c78b05cc0a9a2e1ddc72eaf537dec2197
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-03-16 17:31:44 +0700

    feat(complain): refund request model

--- internal/domain/order/model/refund_request.go
+++ internal/domain/order/model/refund_request.go
@@ -7,10 +7,12 @@ import (
 )
 
 type RefundRequest struct {
-	ID          int       `json:"id"`
-	RequestDate time.Time `json:"requestDate"`
-	Status      string    `json:"status"`
-	InvoiceId   int       `json:"invoiceId"`
+	ID           int       `json:"id"`
+	RequestDate  time.Time `json:"requestDate"`
+	Status       string    `json:"status"`
+	Type         string    `json:"type"`
+	RefundAmount float64   `json:"refundAmount"`
+	InvoiceId    int       `json:"invoiceId"`
 
 	Invoice *InvoicePerShop `json:"invoice"`
 
