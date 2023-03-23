a3455b42a38cf922d6f8b2cf55255637453661af
merisa.syafrina <merisa.syafrina@shopee.com>
2023-03-23 15:21:50 +0700

    feat(get-seller-promotions)

--- internal/domain/shop/dto/shop_promotion.go
+++ internal/domain/shop/dto/shop_promotion.go
@@ -11,10 +11,35 @@ type SellerPromotion struct {
 	Product Product `json:"product"`
 }
 
+func (SellerPromotion) TableName() string {
+	return "shop_promotions"
+}
+
 type Product struct {
 	ID       int                 `json:"id"`
 	Name     string              `json:"name"`
 	Code     string              `json:"code"`
 	ImageURL string              `json:"imageUrl,omitempty"`
 	SKUs     []*productModel.Sku `json:"skus,omitempty"`
+}
+
+type SellerPromotionFilterRequest struct {
+	Limit  int    `form:"limit"`
+	Page   int    `form:"page"`
+	Status string `form:"status"`
+	Name   string `form:"name"`
+}
+
+func (p *SellerPromotionFilterRequest) Validate() {
+	if p.Limit < 1 {
+		p.Limit = 10
+	}
+
+	if p.Page < 1 {
+		p.Page = 1
+	}
+}
+
+func (p *SellerPromotionFilterRequest) Offset() int {
+	return (p.Page - 1) * p.Limit
 }
--- internal/domain/shop/repository/shop_promotion_repository.go
+++ internal/domain/shop/repository/shop_promotion_repository.go
@@ -1,6 +1,15 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"kedai/backend/be-kedai/internal/common/constant"
+	"kedai/backend/be-kedai/internal/domain/shop/dto"
+	"kedai/backend/be-kedai/internal/domain/shop/model"
+	"math"
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type ShopPromotionRepository interface {
 }
@@ -19,6 +28,49 @@ func NewShopPromotionRepository(cfg *ShopPromotionRConfig) ShopPromotionReposito
 	}
 }
 
-func (r *shopPromotionRepositoryImpl) GetSellerPromotions() {
+func (r *shopPromotionRepositoryImpl) GetSellerPromotions(shopId int, request *dto.SellerPromotionFilterRequest) ([]*dto.SellerPromotion, int64, int, error) {
+	var (
+		promotions []*dto.SellerPromotion
+		totalRows  int64
+		totalPages int
+	)
 
+	now := time.Now()
+	query := r.db.Where("shop_id = ?", shopId)
+
+	if request.Name != "" {
+		query = query.Where("shop_promotions.name ILIKE ?", fmt.Sprintf("%%%s%%", request.Name))
+	}
+
+	switch request.Status {
+	case constant.VoucherPromotionStatusOngoing:
+		query = query.Where("shop_promotions.start_from <= ? AND ? < shop_promotions.expired_at", now, now)
+	case constant.VoucherPromotionStatusUpcoming:
+		query = query.Where("? < shop_promotions.start_from", now)
+	case constant.VoucherPromotionStatusExpired:
+		query = query.Where("shop_promotions.expired_at <= ?", now)
+	}
+
+	query = query.Select("shop_promotions.*, "+
+		"CASE WHEN start_period <= ? AND end_period >= ? THEN ? "+
+		"WHEN start_from > ? THEN ? "+
+		"ELSE ? "+
+		"END as status", now, now, constant.VoucherPromotionStatusOngoing, now, constant.VoucherPromotionStatusUpcoming, constant.VoucherPromotionStatusExpired)
+
+	query = query.Session(&gorm.Session{})
+
+	err := query.Model(&model.ShopPromotion{}).Distinct("shop_promotions.id").Count(&totalRows).Error
+	if err != nil {
+		return nil, 0, 0, err
+	}
+
+	totalPages = int(math.Ceil(float64(totalRows) / float64(request.Limit)))
+
+	err = query.
+		Order("shop_promotions.created_at desc").Limit(request.Limit).Offset(request.Offset()).Find(&promotions).Error
+	if err != nil {
+		return nil, 0, 0, err
+	}
+
+	return promotions, totalRows, totalPages, nil
 }
