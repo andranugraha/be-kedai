24130fb8d8641541b6bd39d56578aa360a05b365
merisa.syafrina <merisa.syafrina@shopee.com>
2023-03-27 11:36:23 +0700

    test(get-seller-promotions): service test for get seller promotion by id

--- internal/domain/shop/service/shop_promotion_service_test.go
+++ internal/domain/shop/service/shop_promotion_service_test.go
@@ -69,7 +69,7 @@ func TestGetSellerPromotions(t *testing.T) {
 			},
 		},
 		{
-			description: "should return promotions data when succeed to get vouchers",
+			description: "should return promotions data when succeed to get promotions",
 			input: input{
 				userID:  userID,
 				request: request,
@@ -102,6 +102,93 @@ func TestGetSellerPromotions(t *testing.T) {
 			})
 
 			data, err := shopPromotionService.GetSellerPromotions(tc.input.userID, tc.input.request)
+
+			assert.Equal(t, tc.expected.data, data)
+			assert.Equal(t, tc.expected.err, err)
+		})
+	}
+}
+
+func TestGetSellerPromotionById(t *testing.T) {
+	type input struct {
+		userID      int
+		promotionId int
+	}
+	type expected struct {
+		data *dto.SellerPromotion
+		err  error
+	}
+
+	var (
+		userID      = 1
+		shopID      = 1
+		promotionId = 1
+		promotion   = dto.SellerPromotion{}
+	)
+
+	tests := []struct {
+		description string
+		input
+		beforeTest func(*mocks.ShopService, *mocks.ShopPromotionRepository)
+		expected
+	}{
+		{
+			description: "should return error when failed to get shop",
+			input: input{
+				userID:      userID,
+				promotionId: promotionId,
+			},
+			beforeTest: func(ss *mocks.ShopService, vr *mocks.ShopPromotionRepository) {
+				ss.On("FindShopByUserId", userID).Return(nil, errors.New("failed to get shop"))
+			},
+			expected: expected{
+				data: nil,
+				err:  errors.New("failed to get shop"),
+			},
+		},
+		{
+			description: "should return error when failed to get promotion",
+			input: input{
+				userID:      userID,
+				promotionId: promotionId,
+			},
+			beforeTest: func(ss *mocks.ShopService, vr *mocks.ShopPromotionRepository) {
+				ss.On("FindShopByUserId", userID).Return(&model.Shop{ID: shopID, UserID: userID}, nil)
+				vr.On("GetSellerPromotionById", shopID, promotionId).Return(nil, errors.New("failed to get promotion"))
+			},
+			expected: expected{
+				data: nil,
+				err:  errors.New("failed to get promotion"),
+			},
+		},
+		{
+			description: "should return dto when succeed to get promotion",
+			input: input{
+				userID:      userID,
+				promotionId: promotionId,
+			},
+			beforeTest: func(ss *mocks.ShopService, vr *mocks.ShopPromotionRepository) {
+				ss.On("FindShopByUserId", userID).Return(&model.Shop{ID: shopID, UserID: userID}, nil)
+				vr.On("GetSellerPromotionById", shopID, promotionId).Return(&promotion, nil)
+			},
+			expected: expected{
+				data: &dto.SellerPromotion{},
+				err:  nil,
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			shopService := mocks.NewShopService(t)
+			shopPromotionRepository := mocks.NewShopPromotionRepository(t)
+			tc.beforeTest(shopService, shopPromotionRepository)
+			shopPromotionService := service.NewShopPromotionService(&service.ShopPromotionSConfig{
+				ShopService:             shopService,
+				ShopPromotionRepository: shopPromotionRepository,
+			})
+
+			data, err := shopPromotionService.GetSellerPromotionById(tc.input.userID, tc.input.promotionId)
 
 			assert.Equal(t, tc.expected.data, data)
 			assert.Equal(t, tc.expected.err, err)
