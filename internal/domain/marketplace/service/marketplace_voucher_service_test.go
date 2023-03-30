62589997d718f657213573f54a095fbfb8c2eb67
Adithya Kurniawan <adithya.kurniawan@shopee.com>
2023-03-30 12:41:20 +0700

    test(voucher): service unit test

--- internal/domain/marketplace/service/marketplace_voucher_service_test.go
+++ internal/domain/marketplace/service/marketplace_voucher_service_test.go
@@ -1,6 +1,7 @@
 package service_test
 
 import (
+	errs "kedai/backend/be-kedai/internal/common/error"
 	"kedai/backend/be-kedai/internal/domain/marketplace/dto"
 	"kedai/backend/be-kedai/internal/domain/marketplace/model"
 	"kedai/backend/be-kedai/internal/domain/marketplace/service"
@@ -159,6 +160,69 @@ func TestGetValidForCheckout(t *testing.T) {
 			assert.Equal(t, c.expected.err, err)
 			assert.Equal(t, c.expected.result, result)
 
+		})
+	}
+}
+
+func TestCreateMarketplaceVoucher(t *testing.T) {
+	var(
+		val = true
+		catId = 1
+		payId = 1
+	)
+	type input struct {
+		req     dto.CreateMarketplaceVoucherRequest
+		voucher *model.MarketplaceVoucher
+	}
+	type expected struct {
+		result *model.MarketplaceVoucher
+		err    error
+	}
+	type cases struct {
+		description string
+		input
+		expected
+	}
+
+	for _, tc := range []cases{
+		{
+			description: "should return created voucher or error when called",
+			input: input{
+				req: dto.CreateMarketplaceVoucherRequest{
+					Code: "A",
+					IsHidden: &val,
+					CategoryID: &catId,
+					PaymentMethodID: &payId,
+				},
+				voucher: &model.MarketplaceVoucher{
+					Code: "A",
+					IsHidden: val,
+					CategoryID: &catId,
+					PaymentMethodID: &payId,
+				},
+			},
+			expected: expected{
+				result: &model.MarketplaceVoucher{
+					Code: "A",
+					IsHidden: val,
+					CategoryID: &catId,
+					PaymentMethodID: &payId,
+				},
+				err: errs.ErrInternalServerError,
+			},
+		},
+	} {
+		t.Run(tc.description, func(t *testing.T) {
+			mockRepo := new(mocks.MarketplaceVoucherRepository)
+			mockRepo.On("CreateMarketplaceVoucher", tc.input.voucher).Return(tc.input.voucher, tc.expected.err)
+			service := service.NewMarketplaceVoucherService(&service.MarketplaceVoucherSConfig{
+				MarketplaceVoucherRepository: mockRepo,
+			})
+
+			result, err := service.CreateMarketplaceVoucher(&tc.input.req)
+
+			assert.Equal(t, tc.expected.result, result)
+			assert.Equal(t, tc.expected.err, err)
 		})
 	}
 }
