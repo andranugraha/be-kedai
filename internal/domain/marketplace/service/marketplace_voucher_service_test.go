c014b69985f9e05e43da36b054868228934623c9
merisa.syafrina <merisa.syafrina@shopee.com>
2023-03-30 11:17:33 +0700

    feat(get-,marketplace-voucher): service test

--- internal/domain/marketplace/service/marketplace_voucher_service_test.go
+++ internal/domain/marketplace/service/marketplace_voucher_service_test.go
@@ -61,6 +61,57 @@ func TestGetMarketplaceVoucher(t *testing.T) {
 
 }
 
+func TestGetMarketplaceVoucherAdmin(t *testing.T) {
+	type input struct {
+		req        *dto.GetMarketplaceVoucherRequest
+		err        error
+		beforeTest func(*mocks.MarketplaceVoucherRepository)
+	}
+	type expected struct {
+		result []*model.MarketplaceVoucher
+		err    error
+	}
+
+	cases := []struct {
+		description string
+		input
+		expected
+	}{
+		{
+			description: "should return error and marketplace vouchers",
+			input: input{
+				err: nil,
+				req: &dto.GetMarketplaceVoucherRequest{},
+				beforeTest: func(m *mocks.MarketplaceVoucherRepository) {
+					m.On("GetMarketplaceVoucherAdmin", &dto.GetMarketplaceVoucherRequest{}).Return(nil, nil)
+				},
+			},
+			expected: expected{
+				result: nil,
+				err:    nil,
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.description, func(t *testing.T) {
+			m := mocks.NewMarketplaceVoucherRepository(t)
+			c.beforeTest(m)
+
+			s := service.NewMarketplaceVoucherService(&service.MarketplaceVoucherSConfig{
+				MarketplaceVoucherRepository: m,
+			})
+
+			result, err := s.GetMarketplaceVoucherAdmin(c.input.req)
+
+			assert.Equal(t, c.expected.err, err)
+			assert.Equal(t, c.expected.result, result)
+
+		})
+	}
+
+}
+
 func TestGetValidByUserID(t *testing.T) {
 	type input struct {
 		req        *dto.GetMarketplaceVoucherRequest
