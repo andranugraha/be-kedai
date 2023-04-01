7e20c093e47d95e37d9663bbb95c0a8c9cce4366
nugraha.andra <nugraha.andra@shopee.com>
2023-04-01 22:17:34 +0700

    test(seller): update category test

--- internal/domain/shop/handler/shop_category_handler_test.go
+++ internal/domain/shop/handler/shop_category_handler_test.go
@@ -277,3 +277,114 @@ func TestCreateSellerCategory(t *testing.T) {
 		})
 	}
 }
+
+func TestUpdateSellerCategory(t *testing.T) {
+	var (
+		updateSellerCategoryRequest = dto.UpdateSellerCategoryRequest{
+			Name: "test",
+			ProductIDs: []int{
+				1,
+			},
+		}
+	)
+	tests := []struct {
+		name       string
+		want       response.Response
+		code       int
+		beforeTest func(*mocks.ShopCategoryService)
+	}{
+		{
+			name: "should return 200 when request is valid",
+			want: response.Response{
+				Code:    code.OK,
+				Message: "success",
+				Data: &dto.CreateSellerCategoryResponse{
+					ID: 1,
+				},
+			},
+			code: http.StatusOK,
+			beforeTest: func(shopCategoryService *mocks.ShopCategoryService) {
+				shopCategoryService.On("UpdateSellerCategory", 1, 1, updateSellerCategoryRequest).Return(&dto.CreateSellerCategoryResponse{
+					ID: 1,
+				}, nil)
+			},
+		},
+		{
+			name: "should return 500 when get shop categories failed",
+			want: response.Response{
+				Code:    code.INTERNAL_SERVER_ERROR,
+				Message: errs.ErrInternalServerError.Error(),
+			},
+			code: http.StatusInternalServerError,
+			beforeTest: func(shopCategoryService *mocks.ShopCategoryService) {
+				shopCategoryService.On("UpdateSellerCategory", 1, 1, updateSellerCategoryRequest).Return(nil, errs.ErrInternalServerError)
+			},
+		},
+		{
+			name: "should return 400 when shop not registered",
+			want: response.Response{
+				Code:    code.SHOP_NOT_REGISTERED,
+				Message: errs.ErrShopNotFound.Error(),
+			},
+			code: http.StatusBadRequest,
+			beforeTest: func(shopCategoryService *mocks.ShopCategoryService) {
+				shopCategoryService.On("UpdateSellerCategory", 1, 1, updateSellerCategoryRequest).Return(nil, errs.ErrShopNotFound)
+			},
+		},
+		{
+			name: "should return 400 when product not found",
+			want: response.Response{
+				Code:    code.BAD_REQUEST,
+				Message: errs.ErrProductDoesNotExist.Error(),
+			},
+			code: http.StatusBadRequest,
+			beforeTest: func(shopCategoryService *mocks.ShopCategoryService) {
+				shopCategoryService.On("UpdateSellerCategory", 1, 1, updateSellerCategoryRequest).Return(nil, errs.ErrProductDoesNotExist)
+			},
+		},
+		{
+			name: "should return 409 when category already exists",
+			want: response.Response{
+				Code:    code.DUPLICATE_CATEGORY,
+				Message: errs.ErrCategoryAlreadyExist.Error(),
+			},
+			code: http.StatusConflict,
+			beforeTest: func(shopCategoryService *mocks.ShopCategoryService) {
+				shopCategoryService.On("UpdateSellerCategory", 1, 1, updateSellerCategoryRequest).Return(nil, errs.ErrCategoryAlreadyExist)
+			},
+		},
+		{
+			name: "should return 404 when category not found",
+			want: response.Response{
+				Code:    code.NOT_FOUND,
+				Message: errs.ErrCategoryNotFound.Error(),
+			},
+			code: http.StatusNotFound,
+			beforeTest: func(shopCategoryService *mocks.ShopCategoryService) {
+				shopCategoryService.On("UpdateSellerCategory", 1, 1, updateSellerCategoryRequest).Return(nil, errs.ErrCategoryNotFound)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			expectedBody, _ := json.Marshal(tt.want)
+			payload := test.MakeRequestBody(updateSellerCategoryRequest)
+			shopCategoryService := new(mocks.ShopCategoryService)
+			tt.beforeTest(shopCategoryService)
+			rec := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(rec)
+			c.Set("userId", 1)
+			c.AddParam("categoryId", "1")
+			handler := handler.New(&handler.HandlerConfig{
+				ShopCategoryService: shopCategoryService,
+			})
+
+			c.Request = httptest.NewRequest("PUT", "/sellers/categories/1", payload)
+			handler.UpdateSellerCategory(c)
+
+			assert.Equal(t, tt.code, rec.Code)
+			assert.Equal(t, string(expectedBody), rec.Body.String())
+		})
+	}
+}
