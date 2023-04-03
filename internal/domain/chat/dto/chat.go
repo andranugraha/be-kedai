fd990f9e9742ec89c5f4a952d0ba86152961c868
Muhammad Angga Risfanani <angga.risfanani@shopee.com>
2023-04-03 12:26:22 +0700

    chore(chat): add attribute isFirstMessageOfDay

--- internal/domain/chat/dto/chat.go
+++ internal/domain/chat/dto/chat.go
@@ -61,11 +61,12 @@ type SellerListOfChatResponse struct {
 }
 
 type ChatResponse struct {
-	ID         int       `json:"id"`
-	Message    string    `json:"message"`
-	Time       time.Time `json:"time"`
-	Type       string    `json:"type"`
-	IsIncoming bool      `json:"isIncoming"`
+	ID                  int       `json:"id"`
+	Message             string    `json:"message"`
+	Time                time.Time `json:"time"`
+	Type                string    `json:"type"`
+	IsIncoming          bool      `json:"isIncoming"`
+	IsFirstMessageOfDay *bool     `json:"isFirstMessageOfDay,omitempty"`
 }
 
 func ConvertChatToOutput(c *chatModel.Chat, role string) *ChatResponse {
@@ -79,6 +80,51 @@ func ConvertChatToOutput(c *chatModel.Chat, role string) *ChatResponse {
 		Time:       c.CreatedAt,
 		Type:       c.Type,
 		IsIncoming: incomingSelector(c.Issuer, role),
+	}
+}
+
+func AssignFirstMessagesOfDay(chatResponses []*ChatResponse) { // pointer mutation direcly
+	// Assumption: chat responses ordered by newest to oldest
+	// Reverse slice first
+	for i := 0; i < len(chatResponses)/2; i++ {
+		j := len(chatResponses) - i - 1
+		chatResponses[i], chatResponses[j] = chatResponses[j], chatResponses[i]
+	}
+
+	// Assign true if the message is the first of the day
+	var lastDate time.Time
+	trueVal := true
+	falseVal := false
+	for i, chatResponse := range chatResponses {
+		if i == 0 {
+			chatResponse.IsFirstMessageOfDay = &trueVal
+			lastDate = time.Date(
+				chatResponse.Time.Year(),
+				chatResponse.Time.Month(),
+				chatResponse.Time.Day(),
+				0, 0, 0, 0,
+				time.FixedZone("WIB", 7*60*60)).Add(7 * time.Hour)
+			continue
+		}
+		currentDate := time.Date(
+			chatResponse.Time.Year(),
+			chatResponse.Time.Month(),
+			chatResponse.Time.Day(),
+			0, 0, 0, 0,
+			time.FixedZone("WIB", 7*60*60)).Add(7 * time.Hour)
+		// time comparation in WIB (+07:00)
+		if currentDate.After(lastDate) {
+			chatResponse.IsFirstMessageOfDay = &trueVal
+			lastDate = currentDate
+		} else {
+			chatResponse.IsFirstMessageOfDay = &falseVal
+		}
+	}
+
+	// Reverse slice again
+	for i := 0; i < len(chatResponses)/2; i++ {
+		j := len(chatResponses) - i - 1
+		chatResponses[i], chatResponses[j] = chatResponses[j], chatResponses[i]
 	}
 }
 
